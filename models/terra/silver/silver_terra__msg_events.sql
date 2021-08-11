c651a136fd905f994f56cfa01e75c51d74a3b997
JAMES MISSION <jamesmission@JAMESs-MacBook-Pro.local>
2021-08-11 15:37:55 -0700

    updat unique identifier

--- models/terra/silver/silver_terra__msg_events.sql
+++ models/terra/silver/silver_terra__msg_events.sql
@@ -1,7 +1,7 @@
 {{ 
   config(
     materialized='incremental',
-    unique_key='chain_id || block_id || tx_id || msg_index || event_index', 
+    unique_key='chain_id || block_id || tx_id || msg_index || event_index || event_type', 
     incremental_strategy='delete+insert',
     cluster_by=['block_timestamp', 'block_id', 'tx_id'],
     tags=['snowflake', 'terra_silver_2', 'terra_msg_events']
@@ -14,4 +14,4 @@ WHERE 1=1
 {% if is_incremental() %}
         AND system_created_at >= (select max(system_created_at) from {{source('silver_terra', 'msg_events')}})
 {% endif %}
-QUALIFY(row_number() over(partition by chain_id, block_id, tx_id, msg_index, event_index order by system_created_at desc)) = 1
+QUALIFY(row_number() over(partition by chain_id, block_id, tx_id, msg_index, event_index, event_type order by system_created_at desc)) = 1
