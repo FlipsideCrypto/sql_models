c1eb418c7572d9bd7c939fbf9c38cae2de959999
JAMES MISSION <jamesmission@JAMESs-MacBook-Pro.local>
2021-08-13 07:05:10 -0700

    added semicolon

--- models/terra/silver/silver_terra__transactions.sql
+++ models/terra/silver/silver_terra__transactions.sql
@@ -3,7 +3,7 @@
     materialized='incremental',
     unique_key='chain_id || block_id || tx_id', 
     incremental_strategy='delete+insert',
-    cluster_by=['block_timestamp', 'block_id'],
+    cluster_by=['block_timestamp', 'block_id', 'tx_id'],
     tags=['snowflake', 'terra_silver', 'terra_transactions']
   )
 }}
@@ -12,6 +12,6 @@ select *
 from {{ ref('terra_dbt__transactions')}}
 WHERE 1=1
 {% if is_incremental() %}
-        AND system_created_at::date >= (select dateadd('day',-1,max(system_created_at::date)) from {{source('silver_terra', 'transactions')}})
+        AND system_created_at >= (select max(system_created_at) from {{source('silver_terra', 'transactions')}})
 {% endif %}
-QUALIFY(row_number() over(partition by chain_id, block_id, tx_id order by system_created_at desc)) = 1
+QUALIFY(row_number() over(partition by chain_id, block_id, tx_id order by system_created_at desc)) = 1;
