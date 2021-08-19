ccf7074238e191730f6d0eb3055548cc650af9b0
JAMES MISSION <jamesmission@JAMESs-MacBook-Pro.local>
2021-08-18 19:38:48 -0700

    change column name in config

--- models/ethereum/aave/aave__market_stats.sql
+++ models/ethereum/aave/aave__market_stats.sql
@@ -1,7 +1,7 @@
 {{
   config(
     materialized='incremental',
-    unique_key='block_hour || aave_version || underlying_contract || atoken_price || aave_price || reserve_price',
+    unique_key='block_hour || aave_version || aave_market || atoken_price || aave_price || reserve_price',
     incremental_strategy='delete+insert',
     tags=['snowflake', 'ethereum', 'aave', 'aave_market_stats']
   )
