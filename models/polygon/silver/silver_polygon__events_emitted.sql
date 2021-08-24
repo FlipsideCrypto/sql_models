64f3530b0a3b8462ef431fdaa76b0e2967ddc3a0
JAMES MISSION <jamesmission@JAMESs-MacBook-Pro.local>
2021-08-23 18:01:32 -0700

    updated bronze to silver dedupe logic

--- models/polygon/silver/silver_polygon__events_emitted.sql
+++ models/polygon/silver/silver_polygon__events_emitted.sql
@@ -9,9 +9,13 @@
 }}
 
 select *
+from (
+select *
 from {{ ref('polygon_dbt__events_emitted')}}
 WHERE 1=1
 {% if is_incremental() %}
         AND system_created_at >= (select dateadd('day',-1,max(system_created_at::date)) from {{source('silver_polygon', 'events_emitted')}})
 {% endif %}
+QUALIFY(rank() over(partition by tx_id order by block_id desc)) = 1
+)a
 QUALIFY(row_number() over(partition by chain_id, block_id, tx_id, event_index order by system_created_at desc)) = 1
--- models/polygon/silver/silver_polygon__transactions.sql
+++ models/polygon/silver/silver_polygon__transactions.sql
@@ -14,4 +14,4 @@ WHERE 1=1
 {% if is_incremental() %}
         AND system_created_at >= (select dateadd('day',-1,max(system_created_at::date)) from {{source('silver_polygon', 'transactions')}})
 {% endif %}
-QUALIFY(row_number() over(partition by chain_id, block_id, tx_id order by system_created_at desc)) = 1
+QUALIFY(row_number() over(partition by chain_id, tx_id order by block_id desc, system_created_at desc)) = 1
