7d6e7d28ed488aae0d3b870722d3943392611aa7
JAMES MISSION <jamesmission@JAMESs-MacBook-Pro.local>
2021-08-24 10:41:14 -0700

    make polygon use ref tables

--- models/polygon/polygon__transactions.sql
+++ models/polygon/polygon__transactions.sql
@@ -11,10 +11,10 @@ WITH events AS (
         tx_id
         , sum(case when log_index is not null then 1 else 0 end) as event_count
     FROM
-        {{ source('silver_polygon','udm_events') }}
+        {{ ref('silver_polygon__udm_events')}}
     where 1=1
     {% if is_incremental() %}
-    and block_timestamp::date >= (select max(block_timestamp::date) from {{source('polygon', 'transactions')}})
+    and block_timestamp::date >= (select max(block_timestamp::date) from {{source('polygon', 'udm_events')}})
     {% endif %}
     group by 1
 ),
@@ -22,7 +22,7 @@ txn AS (
   SELECT
   *
   FROM
-  {{source('silver_polygon', 'transactions')}}
+  {{ ref('silver_polygon__transactions')}}
   WHERE 1=1
     {% if is_incremental() %}
     AND block_timestamp::date >= (select max(block_timestamp::date) from {{source('polygon', 'transactions')}})
