93543e795f573e504474e41efc34bf94dd0a559d
yulike <xiuyangguan@gmail.com>
2021-08-26 12:49:44 -0400

    Update the nft tables

--- models/ethereum/nft/rarible_nfts/ethereum_dbt__rarible_sales.sql
+++ models/ethereum/nft/rarible_nfts/ethereum_dbt__rarible_sales.sql
@@ -8,36 +8,26 @@
   )
 }}
 
-WITH token_transfers AS (
+WITH rarible_txns AS (
+  SELECT
+    tx_hash AS tx_id
+  FROM 
+    {{ source('ethereum', 'ethereum_transactions') }} 
+  WHERE to_address IN ('0x9757f2d2b135150bbeb65308d4a91804107cd8d6',
+                              '0xcd4ec7b66fbc029c116ba9ffb3e59351c20b5b06')
+  GROUP BY tx_hash
+),
+token_transfers AS (
   SELECT 
     tx_id, contract_addr AS contract_address, 
     event_inputs:from AS seller, 
     event_inputs:to AS buyer, 
     event_inputs:tokenId AS token_id
   FROM {{ source('ethereum', 'ethereum_events_emitted') }}
-  WHERE event_name = 'Transfer' 
-    AND 
-    tx_id IN (
-
-        SELECT tx_hash 
-        FROM {{ source('ethereum', 'ethereum_events') }} 
-        WHERE input_method IN ('0x9cec6392', '0x9cec639')
-          {% if is_incremental() %}
-            AND block_timestamp >= getdate() - interval '5 days'
-          {% else %}
-            AND block_timestamp >= getdate() - interval '9 months'
-          {% endif %}
-
-    ) 
-    AND token_id IS NOT NULL
-    {% if is_incremental() %}
-       AND block_timestamp >= getdate() - interval '5 days'
-    {% else %}
-       AND block_timestamp >= getdate() - interval '9 months'
-    {% endif %}
-
+  WHERE event_name = 'Transfer' AND
+    tx_id IN (SELECT tx_id FROM rarible_txns) AND
+    token_id IS NOT NULL
   UNION
-
   SELECT 
     tx_id, 
     contract_addr AS contract_address,
@@ -45,29 +35,10 @@ WITH token_transfers AS (
     event_inputs:_to AS buyer, 
     event_inputs:_id AS token_id
   FROM {{ source('ethereum', 'ethereum_events_emitted') }}
-  WHERE event_name = 'TransferSingle' 
-    AND
-    tx_id IN (
-
-        SELECT tx_hash 
-        FROM {{ source('ethereum', 'ethereum_events') }} 
-        WHERE input_method IN ('0x9cec6392', '0x9cec639')
-          {% if is_incremental() %}
-            AND block_timestamp >= getdate() - interval '5 days'
-          {% else %}
-            AND block_timestamp >= getdate() - interval '9 months'
-          {% endif %}
-          
-      ) 
-    AND token_id IS NOT NULL
-    {% if is_incremental() %}
-       AND block_timestamp >= getdate() - interval '5 days'
-    {% else %}
-       AND block_timestamp >= getdate() - interval '9 months'
-    {% endif %}
-
+  WHERE event_name = 'TransferSingle' AND
+    tx_id IN (SELECT tx_id FROM rarible_txns) AND
+    token_id IS NOT NULL
   UNION
-
   SELECT 
     tx_id, 
     contract_addr AS contract_address,
@@ -75,64 +46,45 @@ WITH token_transfers AS (
     event_inputs:_to AS buyer, 
     event_inputs:_tokenId AS token_id
   FROM {{ source('ethereum', 'ethereum_events_emitted') }}
-  WHERE event_name = 'Transfer' 
-    AND
-    tx_id IN (  
-
-        SELECT tx_hash 
-        FROM {{ source('ethereum', 'ethereum_events') }} 
-        WHERE input_method IN ('0x9cec6392', '0x9cec639')
-          {% if is_incremental() %}
-            AND block_timestamp >= getdate() - interval '5 days'
-          {% else %}
-            AND block_timestamp >= getdate() - interval '9 months'
-          {% endif %}
-          
-     ) 
-    AND token_id IS NOT NULL
-    {% if is_incremental() %}
-       AND block_timestamp >= getdate() - interval '5 days'
-    {% else %}
-       AND block_timestamp >= getdate() - interval '9 months'
-    {% endif %}
-
+  WHERE event_name = 'Transfer' AND
+    tx_id IN (SELECT tx_id FROM rarible_txns) AND
+    token_id IS NOT NULL
 ),
 nfts_per_tx AS (
   SELECT tx_id, count(token_id) as n_tokens
   FROM token_transfers
-  GROUP BY 
-    tx_id
+  GROUP BY tx_id
 ),
 tx_buyer_seller AS (
   SELECT tx_id, buyer, seller
   FROM token_transfers
-  WHERE 
-    token_id IS NOT NULL
-  GROUP BY 
-    tx_id, 
-    buyer, 
-    seller
+  WHERE token_id IS NOT NULL
+  GROUP BY tx_id, buyer, seller
 ),
 token_transfer_events AS (
   SELECT 
-    ee.tx_id, 
-    block_timestamp, 
-    origin_address,
-    from_address, 
-    to_address, 
-    symbol, 
-    amount, 
-    buyer, 
-    seller
+  ee.tx_id, 
+  block_timestamp, 
+  origin_address,
+  from_address, 
+  to_address, 
+  symbol,
+  contract_address,
+  amount, 
+  buyer, 
+  seller
   FROM {{ ref('ethereum__udm_events') }} ee
-  JOIN tx_buyer_seller tbs 
-    ON ee.tx_id = tbs.tx_id
-  WHERE 
-    ee.tx_id IN (SELECT tx_id FROM token_transfers WHERE token_id IS NOT NULL)
-    AND 
-    amount > 0 
-    AND 
-    (symbol != 'ETH' OR (symbol = 'ETH' AND from_address = '0xcd4ec7b66fbc029c116ba9ffb3e59351c20b5b06'))
+  JOIN tx_buyer_seller tbs ON ee.tx_id = tbs.tx_id
+  WHERE ee.tx_id IN (SELECT tx_id FROM token_transfers WHERE token_id IS NOT NULL)
+  AND 
+  amount > 0 
+  AND 
+  (symbol != 'ETH' OR (symbol = 'ETH' AND from_address IN ('0xcd4ec7b66fbc029c116ba9ffb3e59351c20b5b06', '0x9757f2d2b135150bbeb65308d4a91804107cd8d6')))
+  {% if is_incremental() %}
+    AND block_timestamp >= getdate() - interval '5 days'
+  {% else %}
+    AND block_timestamp >= getdate() - interval '9 months'
+  {% endif %}
 ),
 fee_tracking AS (
   SELECT
@@ -141,12 +93,19 @@ fee_tracking AS (
     from_address, 
     to_address, 
     symbol, 
+    contract_address,
     amount, 
     buyer, 
     seller, 
     RANK() OVER (PARTITION BY tx_id ORDER BY amount ASC) AS rank_amount,
     count(*) OVER (PARTITION BY tx_id) AS n_transfers
   FROM token_transfer_events
+  WHERE 1=1
+  {% if is_incremental() %}
+    AND block_timestamp >= getdate() - interval '5 days'
+  {% else %}
+    AND block_timestamp >= getdate() - interval '9 months'
+  {% endif %}
 ),
 platform_fee AS (
 SELECT 
@@ -155,11 +114,17 @@ SELECT
     from_address, 
     to_address, 
     symbol, 
+    contract_address,
     amount AS platform_fee, 
     buyer, 
     seller
 FROM fee_tracking
 WHERE rank_amount = 1
+{% if is_incremental() %}
+  AND block_timestamp >= getdate() - interval '5 days'
+{% else %}
+  AND block_timestamp >= getdate() - interval '9 months'
+{% endif %}
 ),
 
 creator_fee AS (
@@ -169,13 +134,17 @@ SELECT
     from_address, 
     to_address, 
     symbol, 
+    contract_address,
     amount AS creator_fee, 
     buyer, 
     seller
 FROM fee_tracking
-WHERE 
-  rank_amount = 2 
-  AND n_transfers = 3
+WHERE rank_amount = 2 AND n_transfers = 3
+{% if is_incremental() %}
+  AND block_timestamp >= getdate() - interval '5 days'
+{% else %}
+  AND block_timestamp >= getdate() - interval '9 months'
+{% endif %}
 ),
 
 sale_price AS (
@@ -183,40 +152,39 @@ SELECT
     tx_id, 
     block_timestamp, 
     symbol, 
+    contract_address,
     sum(amount) AS price
 FROM token_transfer_events
 WHERE buyer = origin_address
+{% if is_incremental() %}
+  AND block_timestamp >= getdate() - interval '5 days'
+{% else %}
+  AND block_timestamp >= getdate() - interval '9 months'
+{% endif %}
 GROUP BY 
-  tx_id, 
-  block_timestamp, 
-  symbol
+tx_id, 
+    block_timestamp, 
+    symbol,
+    contract_address
 )
 
 SELECT
-  'rarible' AS event_platform,
-  tt.tx_id, 
-  sale_price.block_timestamp, 
-  'sale' AS event_type,
-  tt.contract_address,
-  token_id,
-  tt.seller AS event_from,
-  tt.buyer AS event_to, 
-  price / n_tokens AS price,
-  platform_fee / n_tokens AS platform_fee, 
-  creator_fee / n_tokens AS creator_fee,
-  sale_price.symbol AS tx_currency
+'rarible' AS event_platform,
+tt.tx_id, 
+sale_price.block_timestamp, 
+'sale' AS event_type,
+tt.contract_address,
+token_id,
+tt.seller AS event_from,
+tt.buyer AS event_to, 
+COALESCE(price / n_tokens, 0) AS price,
+COALESCE(platform_fee / n_tokens, 0) AS platform_fee, 
+COALESCE(creator_fee / n_tokens, 0) AS creator_fee,
+CASE WHEN sale_price.symbol IS NULL THEN sale_price.contract_address ELSE sale_price.symbol END AS tx_currency
 FROM token_transfers tt
-
-JOIN sale_price 
-  ON tt.tx_id = sale_price.tx_id
-
-JOIN platform_fee 
-  ON tt.tx_id = platform_fee.tx_id
-
-JOIN creator_fee 
-  ON tt.tx_id = creator_fee.tx_id
-  
-JOIN nfts_per_tx 
-  ON tt.tx_id = nfts_per_tx.tx_id
+JOIN sale_price ON tt.tx_id = sale_price.tx_id
+LEFT OUTER JOIN platform_fee ON tt.tx_id = platform_fee.tx_id
+LEFT OUTER JOIN creator_fee ON tt.tx_id = creator_fee.tx_id
+LEFT OUTER JOIN nfts_per_tx ON tt.tx_id = nfts_per_tx.tx_id
 
 
