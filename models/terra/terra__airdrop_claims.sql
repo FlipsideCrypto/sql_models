f336363206cc710c5a4a736b56ed7f45a36732a2
jkallogjeri <41237295+jkallogjeri@users.noreply.github.com>
2021-09-19 20:09:37 -0400

    fix

--- models/terra/terra__airdrop_claims.sql
+++ models/terra/terra__airdrop_claims.sql
@@ -17,10 +17,11 @@ SELECT
   msg_value:contract::string as contract_address,
   l.address_name as contract_label
 FROM {{source('silver_terra', 'msgs')}}
-WHERE msg_value:execute_msg:claim IS NOT NULL
 
 LEFT OUTER JOIN {{source('shared','udm_address_labels_new')}} as l
 ON msg_value:contract::string = l.address
+
+WHERE msg_value:execute_msg:claim IS NOT NULL
 
 {% if is_incremental() %}
     AND block_timestamp::date >= (select max(block_timestamp::date) from {{source('silver_terra', 'msgs')}})
