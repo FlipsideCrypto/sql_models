72c8b922dfffa2bb918b71f65463f30fead07490
Julius Remigio <14811322+juls858@users.noreply.github.com>
2021-10-04 15:23:54 -0700

    update queries to include Terra-5 models

--- models/terra/terra_dbt__transitions.sql
+++ models/terra/terra_dbt__transitions.sql
@@ -1,29 +1,53 @@
-{{ 
-  config(
-    materialized='incremental',
-    unique_key='chain_id || block_id || index || transition_type || event', 
-    incremental_strategy='delete+insert',
-    tags=['snowflake', 'terra_silver', 'terra_transitions']
-  )
-}}
+{{ config(
+  materialized = 'incremental',
+  unique_key = 'chain_id || block_id || index || transition_type || event',
+  incremental_strategy = 'delete+insert',
+  tags = ['snowflake', 'terra_silver', 'terra_transitions']
+) }}
 
-with base_tables as (
-  select *
-  from {{source('bronze', 'prod_terra_sink_645110886')}}
-  where record_content:model:name::string = 'terra_transition_model'
-  {% if is_incremental() %}
-        AND (record_metadata:CreateTime::int/1000)::timestamp::date >= (select dateadd('day',-1,max(system_created_at::date)) from {{source('terra_dbt', 'transitions')}})
-  {% endif %}
-  )
+WITH base_tables AS (
 
-select (record_metadata:CreateTime::int/1000)::timestamp as system_created_at,
-      t.value:blockchain::string as blockchain,
-      t.value:block_id::bigint as block_id,
-      t.value:block_timestamp::timestamp as block_timestamp,
-      t.value:chain_id::string as chain_id,
-      t.value:event::string as event,
-      t.value:index::integer as index,
-      t.value:transition_type::string as transition_type,
-      t.value:attributes::object as event_attributes
-from base_tables
-,lateral flatten(input => record_content:results) t
+  SELECT
+    *
+  FROM
+    {{ source(
+      'bronze',
+      'prod_terra_sink_645110886'
+    ) }}
+  WHERE
+    record_content :model :name :: STRING IN (
+      'terra_transition_model',
+      'terra-5_transition_model'
+    )
+
+{% if is_incremental() %}
+AND (
+  record_metadata :CreateTime :: INT / 1000
+) :: TIMESTAMP :: DATE >= (
+  SELECT
+    DATEADD('day', -1, MAX(system_created_at :: DATE))
+  FROM
+    {{ source(
+      'terra_dbt',
+      'transitions'
+    ) }}
+)
+{% endif %}
+)
+SELECT
+  (
+    record_metadata :CreateTime :: INT / 1000
+  ) :: TIMESTAMP AS system_created_at,
+  t.value :blockchain :: STRING AS blockchain,
+  t.value :block_id :: bigint AS block_id,
+  t.value :block_timestamp :: TIMESTAMP AS block_timestamp,
+  t.value :chain_id :: STRING AS chain_id,
+  t.value :event :: STRING AS event,
+  t.value :index :: INTEGER AS INDEX,
+  t.value :transition_type :: STRING AS transition_type,
+  t.value :attributes :: OBJECT AS event_attributes
+FROM
+  base_tables,
+  LATERAL FLATTEN(
+    input => record_content :results
+  ) t
