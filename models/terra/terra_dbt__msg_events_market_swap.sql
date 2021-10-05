3b327d8394cd8c6030755313272b22648207b0a0
Julius Remigio <14811322+juls858@users.noreply.github.com>
2021-10-05 16:20:04 -0700

    - removed terra_silver as source

--- models/terra/terra_dbt__msg_events_market_swap.sql
+++ models/terra/terra_dbt__msg_events_market_swap.sql
@@ -1,34 +1,35 @@
-{{ 
-  config(
-    materialized='incremental', 
-    sort='block_timestamp', 
-    unique_key='block_id', 
-    incremental_strategy='delete+insert',
-    cluster_by=['block_timestamp'],
-    tags=['snowflake', 'terra', 'swap']
-  )
-}}
-SELECT 
+{{ config(
+  materialized = 'incremental',
+  sort = 'block_timestamp',
+  unique_key = 'block_id',
+  incremental_strategy = 'delete+insert',
+  cluster_by = ['block_timestamp'],
+  tags = ['snowflake', 'terra', 'swap']
+) }}
+
+SELECT
   blockchain,
   chain_id,
   tx_status,
   block_id,
-  block_timestamp, 
-  tx_id, 
+  block_timestamp,
+  tx_id,
   tx_type,
   msg_module,
-  msg_type, 
+  msg_type,
   event_type,
   event_attributes,
-  event_attributes:offer::string AS offer,
-  event_attributes:swap_coin::string AS swap_coin,
-  event_attributes:swap_fee::string AS swap_fee,
-  event_attributes:trader::string AS trader
-FROM {{source('silver_terra', 'msg_events')}} 
-WHERE msg_module = 'market'
-AND event_type = 'swap'
+  event_attributes :offer :: STRING AS offer,
+  event_attributes :swap_coin :: STRING AS swap_coin,
+  event_attributes :swap_fee :: STRING AS swap_fee,
+  event_attributes :trader :: STRING AS trader
+FROM
+  {{ ref('silver_terra__msg_events') }}
+WHERE
+  msg_module = 'market'
+  AND event_type = 'swap'
+
 {% if is_incremental() %}
- AND block_timestamp >= getdate() - interval '1 days'
--- {% else %}
+AND block_timestamp >= getdate() - INTERVAL '1 days' -- {% else %}
 --  AND block_timestamp >= getdate() - interval '9 months'
 {% endif %}
