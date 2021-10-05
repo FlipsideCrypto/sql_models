f261ec51b3903f20e3ec932e0960d32a1ccf38b2
JAMES MISSION <jamesmission@JAMESs-MacBook-Pro.local>
2021-10-05 16:18:04 -0700

    track read ui changes

--- models/ethereum/ethereum__reads.sql
+++ models/ethereum/ethereum__reads.sql
@@ -1,8 +1,7 @@
 {{ 
   config(
-    materialized='incremental', 
-    sort='block_timestamp', 
-    unique_key='block_id', 
+    materialized='incremental',
+    unique_key='block_id || contract_address || function_name || inputs', 
     incremental_strategy='delete+insert',
     cluster_by=['block_timestamp', 'contract_address'],
     tags=['snowflake', 'ethereum', 'reads']
@@ -19,7 +18,8 @@ SELECT
   PROJECT_ID AS project_id,
   b.PROJECT_NAME AS project_name,
   VALUE_NUMERIC AS value_numeric,
-  VALUE_STR AS value_string
+  VALUE_STR AS value_string,
+  null::object as value_object
 FROM {{ source('ethereum', 'ethereum_reads') }} b
 
 LEFT OUTER JOIN {{ source('ethereum', 'ethereum_address_labels') }} as contract_labels
@@ -30,4 +30,27 @@ WHERE
   b.block_timestamp >= getdate() - interval '40 hours'
 {% else %}
   b.block_timestamp >= getdate() - interval '9 months'
+{% endif %}
+
+UNION
+
+select block_id,
+block_timestamp,
+contract_address,
+coalesce(l.address_name, contract_name) as contract_name,
+function_name,
+inputs,
+project_id,
+r.project_name,
+value_numeric,
+value_string,
+value_object as value_object
+from {{ ref('silver_ethereum__reads')}} r
+left join {{ source('ethereum', 'ethereum_address_labels') }} as l
+on r.contract_address = l.address
+WHERE 
+{% if is_incremental() %}
+  r.block_timestamp >= getdate() - interval '40 hours'
+{% else %}
+  r.block_timestamp >= getdate() - interval '9 months'
 {% endif %}
