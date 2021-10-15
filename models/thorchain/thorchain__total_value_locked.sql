be299e584ce2079828e5d815238041b7f12a3d3e
yulike <xiuyangguan@gmail.com>
2021-10-15 14:55:10 -0400

    small thing

--- models/thorchain/thorchain__total_value_locked.sql
+++ models/thorchain/thorchain__total_value_locked.sql
@@ -69,9 +69,9 @@ total_value_pooled_tbl AS (
 
 SELECT 
   COALESCE(total_value_bonded_tbl.day, total_value_pooled_tbl.day) AS day,
-  total_value_pooled,
-  SUM(total_value_bonded) OVER (ORDER BY total_value_bonded_tbl.day ASC) AS total_value_bonded,
-  total_value_pooled + SUM(total_value_bonded) OVER (ORDER BY total_value_bonded_tbl.day ASC) AS total_value_locked
+  COALESCE(total_value_pooled, 0) AS total_value_pooled,
+  COALESCE(SUM(total_value_bonded) OVER (ORDER BY total_value_bonded_tbl.day ASC), 0) AS total_value_bonded,
+  COALESCE(total_value_pooled + SUM(total_value_bonded) OVER (ORDER BY total_value_bonded_tbl.day ASC), 0) AS total_value_locked
 FROM total_value_bonded_tbl
 
 FULL JOIN total_value_pooled_tbl
