96aed29ea7164f7832e560e026c2901dafe5b1f5
jhuhnke <jessica@flipsidecrypto.com>
2021-10-19 14:39:13 -0500

    Changed events function - previously was trying to join object with a string

--- models/terra/anchor__bonds.sql
+++ models/terra/anchor__bonds.sql
@@ -1,11 +1,3 @@
-{{ config(
-    materialized = 'incremental',
-    unique_key = 'block_id || tx_id',
-    incremental_strategy = 'delete+insert',
-    cluster_by = ['block_timestamp', 'block_id'],
-    tags=['snowflake', 'terra', 'anchor', 'bonds']
-) }}
-
 WITH prices AS (
 
   SELECT 
@@ -13,14 +5,8 @@ WITH prices AS (
       currency,
       symbol,
       avg(price_usd) as price
-    FROM {{ ref('terra__oracle_prices')}} 
-
+    FROM FLIPSIDE_DEV_DB.terra.oracle_prices 
     WHERE 1=1
-
-    {% if is_incremental() %}
-    AND block_timestamp::date >= (select max(block_timestamp::date) from {{ref('silver_terra__msgs')}})
-    {% endif %}
-
     GROUP BY 1,2,3
 
 ),
@@ -40,9 +26,9 @@ SELECT
   msg_value:execute_msg:bond:validator::string AS validator,
   msg_value:contract::string AS contract_address,
   l.address_name AS contract_label
-FROM {{ref('silver_terra__msgs')}} m
+FROM FLIPSIDE_DEV_DB.silver_terra.msgs m
 
-LEFT OUTER JOIN {{source('shared','udm_address_labels_new')}} as l
+LEFT OUTER JOIN FLIPSIDE_DEV_DB.silver.udm_address_labels_new as l
 ON msg_value:contract::string = l.address
 
 LEFT OUTER JOIN prices o
@@ -51,35 +37,23 @@ LEFT OUTER JOIN prices o
 
 WHERE msg_value:execute_msg:bond IS NOT NULL
   AND tx_status = 'SUCCEEDED'
-  
-  {% if is_incremental() %}
-    AND block_timestamp::date >= (select max(block_timestamp::date) from {{ref('silver_terra__msgs')}})
-  {% endif %}
-
-),
+), 
 
 events AS (
 
-SELECT 
-  tx_id,
-  event_attributes:"1_amount" / POW(10,6) AS minted_amount,
-  minted_amount * price AS minted_amount_usd, 
-  event_attributes:"1_contract_address"::string as minted_currency
-FROM {{ref('silver_terra__msg_events')}}
+SELECT  
+    tx_id,
+    event_attributes:"exchange_rate" / POW(10,6) AS minted_amount,
+    minted_amount * price AS minted_amount_usd, 
+    event_attributes:"denom"::string as minted_currency
+FROM FLIPSIDE_DEV_DB.silver_terra.msg_events
 
 LEFT OUTER JOIN prices o
  ON date_trunc('hour', block_timestamp) = o.hour
- AND event_attributes:"1_contract_address"::string = o.currency 
-
-WHERE tx_id IN(SELECT tx_id FROM msgs)
-  AND event_type = 'from_contract'
-  AND tx_status = 'SUCCEEDED'
-  AND minted_currency IS NOT NULL
-  AND minted_amount IS NOT NULL
-
-  {% if is_incremental() %}
-    AND block_timestamp::date >= (select max(block_timestamp::date) from {{ref('silver_terra__msgs')}})
-  {% endif %}
+ AND event_attributes:"denom"::string = o.currency 
+ 
+ 
+WHERE tx_status = 'SUCCEEDED'
 
 )
 
@@ -88,18 +62,15 @@ SELECT
   chain_id,
   block_id,
   block_timestamp,
-  m.tx_id,
+  m.tx_id
   sender,
   bonded_amount,
   bonded_amount_usd,
   bonded_currency,
   validator,
-  minted_amount,
-  minted_amount_usd,
-  minted_currency,
   contract_address,
   contract_label
-FROM msgs m 
+FROM msgs m
 
 JOIN events e 
   ON m.tx_id = e.tx_id
