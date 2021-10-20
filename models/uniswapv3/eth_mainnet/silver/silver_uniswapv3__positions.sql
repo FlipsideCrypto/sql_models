97ac02434cba1c358e4c13682c14c63288903c40
drakedanner <drake.danner@gmail.com>
2021-10-20 15:37:22 -0400

    updating silver positions to include old data

--- models/uniswapv3/eth_mainnet/silver/silver_uniswapv3__positions.sql
+++ models/uniswapv3/eth_mainnet/silver/silver_uniswapv3__positions.sql
@@ -6,12 +6,32 @@
   tags = ['snowflake', 'uniswapv3_silver', 'silver_uniswapv3__positions']
 ) }}
 
-SELECT
-  *
-FROM
-  {{ ref('uniswapv3_dbt__positions') }}
-WHERE
-  1 = 1
+WITH silver AS (
+
+  SELECT
+    system_created_at,
+    blockchain,
+    block_id,
+    block_timestamp,
+    fee,
+    fee_growth_inside0_last_x128,
+    fee_growth_inside1_last_x128,
+    is_active,
+    liquidity,
+    liquidity_adjusted,
+    liquidity_provider,
+    nf_position_manager_address,
+    nf_token_id,
+    pool_address,
+    tick_lower,
+    tick_upper,
+    tokens_owed0,
+    tokens_owed1,
+    tx_id
+  FROM
+    {{ ref('uniswapv3_dbt__positions') }}
+  WHERE
+    1 = 1
 
 {% if is_incremental() %}
 AND system_created_at :: DATE >= (
@@ -21,7 +41,63 @@ AND system_created_at :: DATE >= (
     {{ this }} AS positions
 )
 {% endif %}
+UNION ALL
+SELECT
+  '2000-01-01' :: TIMESTAMP AS system_created_at,
+  blockchain,
+  block_id,
+  block_timestamp,
+  fee,
+  fee_growth_inside0_last_x128,
+  fee_growth_inside1_last_x128,
+  is_active,
+  liquidity,
+  liquidity_adjusted,
+  liquidity_provider,
+  nf_position_manager_address,
+  nf_token_id,
+  pool_address,
+  tick_lower,
+  tick_upper,
+  tokens_owed0,
+  tokens_owed1,
+  tx_id
+FROM
+  {{ source(
+    'uniswapv3_eth',
+    'uniswapv3_positions'
+  ) }}
 
-qualify(ROW_NUMBER() over(PARTITION BY nf_token_id, tx_id
+{% if is_incremental() %}
+AND system_created_at :: DATE >= (
+  SELECT
+    DATEADD('day', -1, MAX(system_created_at :: DATE))
+  FROM
+    {{ this }} AS positions
+)
+{% endif %}
+)
+SELECT
+  system_created_at,
+  blockchain,
+  block_id,
+  block_timestamp,
+  fee,
+  fee_growth_inside0_last_x128,
+  fee_growth_inside1_last_x128,
+  is_active,
+  liquidity,
+  liquidity_adjusted,
+  liquidity_provider,
+  nf_position_manager_address,
+  nf_token_id,
+  pool_address,
+  tick_lower,
+  tick_upper,
+  tokens_owed0,
+  tokens_owed1,
+  tx_id
+FROM
+  silver qualify(ROW_NUMBER() over(PARTITION BY tx_id, nf_token_id
 ORDER BY
   system_created_at DESC)) = 1
