af29be1bbc00a87982cdb55755211060cc9437fa
drakedanner <drake.danner@gmail.com>
2021-10-20 15:29:10 -0400

    updated silver pools

--- models/uniswapv3/eth_mainnet/silver/silver_uniswapv3__pools.sql
+++ models/uniswapv3/eth_mainnet/silver/silver_uniswapv3__pools.sql
@@ -6,12 +6,34 @@
   tags = ['snowflake', 'uniswapv3_silver', 'silver_uniswapv3__pools']
 ) }}
 
-SELECT
-  *
-FROM
-  {{ ref('uniswapv3_dbt__pools') }}
-WHERE
-  1 = 1
+WITH silver AS (
+
+  SELECT
+    system_created_at,
+    block_id,
+    block_timestamp,
+    blockchain,
+    factory_address,
+    fee,
+    init_price,
+    init_sqrt_price_x96,
+    init_tick,
+    pool_address,
+    pool_name,
+    tick_spacing,
+    token0,
+    token0_decimals,
+    token0_name,
+    token0_symbol,
+    token1,
+    token1_decimals,
+    token1_name,
+    token1_symbol,
+    tx_id
+  FROM
+    {{ ref('uniswapv3_dbt__pools') }}
+  WHERE
+    1 = 1
 
 {% if is_incremental() %}
 AND system_created_at :: DATE >= (
@@ -21,7 +43,67 @@ AND system_created_at :: DATE >= (
     {{ this }} AS pools
 )
 {% endif %}
+UNION ALL
+SELECT
+  '2000-01-01' :: TIMESTAMP AS system_created_at,
+  block_id,
+  block_timestamp,
+  blockchain,
+  factory_address,
+  fee,
+  init_price,
+  init_sqrt_price_x96,
+  init_tick,
+  pool_address,
+  pool_name,
+  tick_spacing,
+  token0,
+  token0_decimals,
+  token0_name,
+  token0_symbol,
+  token1,
+  token1_decimals,
+  token1_name,
+  token1_symbol,
+  tx_id
+FROM
+  {{ source(
+    'uniswapv3_eth',
+    'uniswapv3_pools'
+  ) }}
 
-qualify(ROW_NUMBER() over(PARTITION BY pool_address
+{% if is_incremental() %}
+AND system_created_at :: DATE >= (
+  SELECT
+    DATEADD('day', -1, MAX(system_created_at :: DATE))
+  FROM
+    {{ this }} AS pools
+)
+{% endif %}
+)
+SELECT
+  system_created_at,
+  block_id,
+  block_timestamp,
+  blockchain,
+  factory_address,
+  fee,
+  init_price,
+  init_sqrt_price_x96,
+  init_tick,
+  pool_address,
+  pool_name,
+  tick_spacing,
+  token0,
+  token0_decimals,
+  token0_name,
+  token0_symbol,
+  token1,
+  token1_decimals,
+  token1_name,
+  token1_symbol,
+  tx_id
+FROM
+  silver qualify(ROW_NUMBER() over(PARTITION BY pool_address
 ORDER BY
   system_created_at DESC)) = 1
