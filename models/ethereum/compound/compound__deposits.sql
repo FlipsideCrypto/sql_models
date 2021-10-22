f102dc9e6bfd9e9aff4930f570d368b76e3f4d01
Nick <nick@flipsidecrypto.com>
2021-10-22 16:26:07 -0400

    compound updates

--- models/ethereum/compound/compound__deposits.sql
+++ models/ethereum/compound/compound__deposits.sql
@@ -12,27 +12,27 @@
 -- pull all ctoken addresses and corresponding name
 WITH ctoks as (
   SELECT
-      DISTINCT contract_address as address,
-      CASE WHEN contract_address = '0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e' THEN 'cBAT'
-          WHEN contract_address = '0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4' THEN 'cCOMP'
-          WHEN contract_address = '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643' THEN 'cDAI'
-          WHEN contract_address = '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5' THEN 'cETH'
-          WHEN contract_address = '0x158079ee67fce2f58472a96584a73c7ab9ac95c1' THEN 'cREP'
-          WHEN contract_address = '0xf5dce57282a584d2746faf1593d3121fcac444dc' THEN 'cSAI'
-          WHEN contract_address = '0x35a18000230da775cac24873d00ff85bccded550' THEN 'cUNI'
-          WHEN contract_address = '0x39aa39c021dfbae8fac545936693ac917d5e7563' THEN 'cUSDC'
-          WHEN contract_address = '0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9' THEN 'cUSDT'
-          WHEN contract_address = '0xc11b1268c1a384e55c48c2391d8d480264a3a7f4' THEN 'cWBTC'
-          WHEN contract_address = '0xccf4429db6322d5c611ee964527d42e5d685dd6a' THEN 'cWBTC2'
-          WHEN contract_address = '0xe65cdb6479bac1e22340e4e755fae7e509ecd06c' THEN 'cAAVE'
-          WHEN contract_address = '0xface851a4921ce59e912d19329929ce6da6eb0c7' THEN 'cLINK'
-          WHEN contract_address = '0x95b4ef2869ebd94beb4eee400a99824bf5dc325b' THEN 'cMKR'
-          WHEN contract_address = '0x4b0181102a0112a2ef11abee5563bb4a3176c9d7' THEN 'cSUSHI'
-          WHEN contract_address = '0x80a2ae356fc9ef4305676f7a3e2ed04e12c33946' THEN 'cYFI'
-          WHEN contract_address = '0x12392f67bdf24fae0af363c24ac620a2f67dad86' THEN 'cTUSD'
-          WHEN contract_address = '0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407' THEN 'cZRX' end project_name
+      DISTINCT contract_addr as address,
+      CASE WHEN contract_addr = '0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e' THEN 'cBAT'
+          WHEN contract_addr = '0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4' THEN 'cCOMP'
+          WHEN contract_addr = '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643' THEN 'cDAI'
+          WHEN contract_addr = '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5' THEN 'cETH'
+          WHEN contract_addr = '0x158079ee67fce2f58472a96584a73c7ab9ac95c1' THEN 'cREP'
+          WHEN contract_addr = '0xf5dce57282a584d2746faf1593d3121fcac444dc' THEN 'cSAI'
+          WHEN contract_addr = '0x35a18000230da775cac24873d00ff85bccded550' THEN 'cUNI'
+          WHEN contract_addr = '0x39aa39c021dfbae8fac545936693ac917d5e7563' THEN 'cUSDC'
+          WHEN contract_addr = '0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9' THEN 'cUSDT'
+          WHEN contract_addr = '0xc11b1268c1a384e55c48c2391d8d480264a3a7f4' THEN 'cWBTC'
+          WHEN contract_addr = '0xccf4429db6322d5c611ee964527d42e5d685dd6a' THEN 'cWBTC2'
+          WHEN contract_addr = '0xe65cdb6479bac1e22340e4e755fae7e509ecd06c' THEN 'cAAVE'
+          WHEN contract_addr = '0xface851a4921ce59e912d19329929ce6da6eb0c7' THEN 'cLINK'
+          WHEN contract_addr = '0x95b4ef2869ebd94beb4eee400a99824bf5dc325b' THEN 'cMKR'
+          WHEN contract_addr = '0x4b0181102a0112a2ef11abee5563bb4a3176c9d7' THEN 'cSUSHI'
+          WHEN contract_addr = '0x80a2ae356fc9ef4305676f7a3e2ed04e12c33946' THEN 'cYFI'
+          WHEN contract_addr = '0x12392f67bdf24fae0af363c24ac620a2f67dad86' THEN 'cTUSD'
+          WHEN contract_addr = '0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407' THEN 'cZRX' end project_name
       FROM {{ref('silver_ethereum__events_emitted')}}
-      WHERE contract_address in (
+      WHERE contract_addr in (
       '0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e', -- cbat
       '0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4', -- ccomp
       '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', -- cdai
@@ -123,12 +123,12 @@ prices AS (
 SELECT 
   DISTINCT block_id,
     block_timestamp,
-    ee.contract_address AS ctoken, 
+    ee.contract_addr AS ctoken, 
     ctoks.project_name AS ctoken_symbol,
     event_inputs:mintTokens/pow(10,d.decimals) AS issued_ctokens,
     event_inputs:mintAmount/pow(10,p.token_decimals) AS supplied_base_asset, 
     event_inputs:mintAmount*p.token_price/pow(10,p.token_decimals) AS supplied_base_asset_usd,
-    CASE WHEN p.token_contract = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' THEN NULL ELSE p.token_contract END AS supplied_contract_address,
+    CASE WHEN p.token_contract = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' THEN NULL ELSE p.token_contract END AS supplied_contract_addr,
     CASE WHEN p.token_contract = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' THEN 'ETH' ELSE p.symbol END AS supplied_symbol,
     REGEXP_REPLACE(event_inputs:minter,'\"','') AS supplier,
     tx_id
@@ -136,20 +136,20 @@ FROM {{ ref('silver_ethereum__events_emitted') }} ee
 LEFT JOIN
 prices p
     ON date_trunc('hour',ee.block_timestamp) = p.block_hour 
-    AND ee.contract_address = p.address
+    AND ee.contract_addr = p.address
 LEFT OUTER JOIN
 ctoks 
-    ON ee.contract_address = ctoks.address
+    ON ee.contract_addr = ctoks.address
 LEFT OUTER JOIN
 ctok_decimals d
-    ON ee.contract_address = d.ctok_address
+    ON ee.contract_addr = d.ctok_address
 WHERE
     {% if is_incremental() %}
     block_timestamp >= getdate() - interval '2 days'
     {% else %}
     block_timestamp >= getdate() - interval '9 months'
     {% endif %}   
-    AND ee.contract_address IN (select address from ctoks)
+    AND ee.contract_addr IN (select address from ctoks)
     AND ee.event_name = 'Mint'
     AND p.token_decimals IS NOT NULL
     AND ctoks.address IS NOT NULL
