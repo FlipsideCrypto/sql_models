cdf29dd2a70296172be68a84e4d421f7c2dcb634
JAMES MISSION <jamesmission@JAMESs-MacBook-Pro.local>
2021-10-22 14:41:55 -0700

    took out to on dbt table

--- models/ethereum/ethereum_dbt__events.sql
+++ models/ethereum/ethereum_dbt__events.sql
@@ -1,6 +1,6 @@
 {{ config(
   materialized = 'incremental',
-  unique_key = 'block_id || tx_hash || log_index || to',
+  unique_key = 'block_id || tx_hash || log_index',
   incremental_strategy = 'delete+insert',
   tags = ['snowflake', 'silver_ethereum', 'ethereum_dbt__events']
 ) }}
