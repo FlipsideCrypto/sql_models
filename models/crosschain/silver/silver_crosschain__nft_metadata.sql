b1898fbacd13fea132fd99b74ea7c64e150cfb12
drakedanner <drake.danner@gmail.com>
2021-10-22 21:09:50 -0400

    grab old silver data

--- models/crosschain/silver/silver_crosschain__nft_metadata.sql
+++ models/crosschain/silver/silver_crosschain__nft_metadata.sql
@@ -38,6 +38,37 @@ AND system_created_at :: DATE >= (
 )
 {% endif %}
 UNION ALL
+SELECT
+    blockchain,
+    commission_rate,
+    contract_address,
+    contract_name,
+    created_at_block_id,
+    created_at_timestamp,
+    created_at_tx_id,
+    creator_address,
+    creator_name,
+    image_url,
+    project_name,
+    token_id,
+    token_metadata,
+    token_metadata_uri,
+    token_name
+FROM
+    {{ source(
+        'ethereum',
+        'nft_metadata'
+    ) }}
+WHERE
+    blockchain = 'ethereum'
+    AND
+
+{% if is_incremental() %}
+created_at_timestamp >= getdate() - INTERVAL '1 days'
+{% else %}
+    created_at_timestamp >= getdate() - INTERVAL '9 months'
+{% endif %}
+UNION ALL
     -- THIS SECTION CURRENTLY PULLS GALACTIC PUNK METADATA ONLY
     -- UNION IN OTHER METADATA AS NEEDED
 SELECT
