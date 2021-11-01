6b2185326fadb3a8dcd1c3470eb714bf5b787d2d
jarrel-b <jarrel.biscocho@gmail.com>
2021-11-01 09:59:40 -0700

    Add near_reward_flow

--- models/gold/gold__near_reward_flow.sql
+++ models/gold/gold__near_reward_flow.sql
@@ -1,11 +1,10 @@
 {{ config(
   materialized='incremental',
-  unique_key='block_timestamp',
+  unique_key='date',
   incremental_strategy='delete+insert',
-  cluster_by=['block_timestamp'],
-  tags=['snowflake', 'gold', 'near', 'gold__near_validators']
+  cluster_by=['date'],
+  tags=['snowflake', 'gold', 'near', 'gold__near_reward_flow'],
 )}}
-
 
 WITH near_labels AS (
     SELECT 
@@ -22,44 +21,42 @@ WITH near_labels AS (
     WHERE blockchain = 'near'
 ), near_prices AS (
     SELECT
-        date_trunc('hour', recorded_at) as hour,
-        price
+      symbol,
+      date_trunc('day', recorded_at) as day,
+      avg(price) as price
     FROM
     {{ source(
         'shared',
         'prices'
     )}}
     WHERE symbol = 'NEAR'
+    GROUP BY symbol, day
 )
 SELECT
   'near' as blockchain,
-  v.block_timestamp,
-  v.block_id as block_number,
-  v.account_id as address,
+  xfer_date as date,
+  entity_id as address,
   address_labels.l1_label as address_label_type,
   address_labels.l2_label as address_label_subtype,
   address_labels.project_name as address_label,
   address_labels.address_name as address_name,
-  v.stake / 10e24 as stake_amount, 
-  (v.stake / 10e24) * stake_price.price as stake_amount_usd,
-  v.is_slashed,
-  v.status,
-  v.expected_blocks, 
-  v.produced_blocks,
-  v.epoch_start_block
+  metric_slug,
+  metric_value,
+  metric_value * p.price as metric_value_usd
 FROM
-  {{ source('near', 'near_validators')}} v
+  {{ source('near', 'near_daily_reward_flow')}} f
 LEFT OUTER JOIN
   near_labels as address_labels
 ON
-  v.account_id = address_labels.address
+  f.entity_id = address_labels.address
 LEFT OUTER JOIN
-  near_prices as stake_price
+  near_prices p
 ON
-  stake_price.hour = date_trunc('hour', v.block_timestamp)
+  p.day = xfer_date
+  AND p.symbol = 'NEAR'
 WHERE
   {% if is_incremental() %}
-    block_timestamp >= getdate() - interval '3 days'
+    xfer_date >= getdate() - interval '3 days'
   {% else %}
-    block_timestamp >= getdate() - interval '9 months'
+    xfer_date >= getdate() - interval '9 months'
   {% endif %}
