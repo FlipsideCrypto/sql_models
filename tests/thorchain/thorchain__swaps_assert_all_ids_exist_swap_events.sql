7272dbc445e3639333989313c6e1b20ce11f1765
flipside-kellen <kellen@flipsidecrypto.com>
2021-11-03 10:09:55 -0700

    updates

--- tests/thorchain/thorchain__swaps_assert_all_ids_exist_swap_events.sql
+++ tests/thorchain/thorchain__swaps_assert_all_ids_exist_swap_events.sql
@@ -1,1 +1,1 @@
-{{ all_ids_exist(ref("thorchain__swap_events"), "tx", ref("thorchain__swaps"), "tx_id") }}
+{{ all_ids_exist(ref("thorchain__swap_events"), "tx_id", ref("thorchain__swaps"), "tx_id") }}
