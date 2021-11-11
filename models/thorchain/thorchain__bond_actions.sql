abebb266f5714f993f94c0d5eedae6b4267eeee4
Julius Remigio <14811322+juls858@users.noreply.github.com>
2021-11-11 12:50:13 -0800

    - left join to thorchain_prices instead of inner - null test for prices_usd except today

--- models/thorchain/thorchain__bond_actions.sql
+++ models/thorchain/thorchain__bond_actions.sql
@@ -1,31 +1,35 @@
-{{ 
-  config(
-    materialized='incremental', 
-    sort='block_timestamp', 
-    unique_key='event_id', 
-    incremental_strategy='delete+insert',
-    tags=['snowflake', 'thorchain', 'thorchain_bond_actions']
-  )
-}}
+{{ config(
+  materialized = 'incremental',
+  sort = 'block_timestamp',
+  unique_key = 'event_id',
+  incremental_strategy = 'delete+insert',
+  tags = ['snowflake', 'thorchain', 'thorchain_bond_actions']
+) }}
 
 WITH block_prices AS (
+
   SELECT
     AVG(rune_usd) AS rune_usd,
     block_id
-  FROM {{ ref('thorchain__prices') }}
-  GROUP BY block_id
+  FROM
+    {{ ref('thorchain__prices') }}
+  GROUP BY
+    block_id
 ),
-
 bond_events AS (
-  SELECT * FROM {{ ref('thorchain__bond_events') }}
-  WHERE TRUE
-    {% if is_incremental() %}
-    AND block_timestamp >= getdate() - interval '2 days'
-    {% else %}
-    AND block_timestamp >= getdate() - interval '9 months'
-    {% endif %}
-)
+  SELECT
+    *
+  FROM
+    {{ ref('thorchain__bond_events') }}
+  WHERE
+    TRUE
 
+{% if is_incremental() %}
+AND block_timestamp >= getdate() - INTERVAL '2 days'
+{% else %}
+  AND block_timestamp >= getdate() - INTERVAL '9 months'
+{% endif %}
+)
 SELECT
   be.block_timestamp,
   be.block_id,
@@ -36,9 +40,9 @@ SELECT
   asset,
   blockchain,
   bond_type,
-  COALESCE(e8 / POW(10, 8), 0) AS asset_amount,
-  COALESCE(rune_usd * asset_amount / POW(10, 8), 0) AS asset_usd
-FROM bond_events be
-
-JOIN block_prices p 
-ON be.block_id = p.block_id
+  COALESCE(e8 / pow(10, 8), 0) AS asset_amount,
+  COALESCE(rune_usd * asset_amount / pow(10, 8), 0) AS asset_usd
+FROM
+  bond_events be
+  LEFT JOIN block_prices p
+  ON be.block_id = p.block_id
