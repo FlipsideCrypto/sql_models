7f0810217b990d1a9a028064e19147ef5daa3d25
Julius Remigio <14811322+juls858@users.noreply.github.com>
2021-11-11 16:34:03 -0800

    AN-306/thorchain-midgard-upgrade (#161)

--- models/thorchain/thorchain__liquidity_actions.sql
+++ models/thorchain/thorchain__liquidity_actions.sql
@@ -1,82 +1,110 @@
-{{ 
-  config(
-    materialized='incremental', 
-    sort='block_timestamp', 
-    unique_key='event_id', 
-    incremental_strategy='delete+insert',
-    tags=['snowflake', 'thorchain', 'thorchain_liquidity_actions']
-  )
-}}
+{{ config(
+  materialized = 'incremental',
+  sort = 'block_timestamp',
+  unique_key = 'event_id',
+  incremental_strategy = 'delete+insert',
+  tags = ['snowflake', 'thorchain', 'thorchain_liquidity_actions']
+) }}
 
 WITH stakes AS (
-  SELECT * FROM {{ ref('thorchain__stake_events') }}
-  WHERE TRUE
-    {% if is_incremental() %}
-    AND block_timestamp >= getdate() - interval '2 days'
-    {% else %}
-    AND block_timestamp >= getdate() - interval '9 months'
-    {% endif %}
+
+  SELECT
+    *
+  FROM
+    {{ ref('thorchain__stake_events') }}
+  WHERE
+    TRUE
+
+{% if is_incremental() %}
+AND block_timestamp >= getdate() - INTERVAL '2 days'
+{% else %}
+  AND block_timestamp >= getdate() - INTERVAL '9 months'
+{% endif %}
 ),
 unstakes AS (
-  SELECT * FROM {{ ref('thorchain__unstake_events') }}
-  WHERE TRUE
-    {% if is_incremental() %}
-    AND block_timestamp >= getdate() - interval '2 days'
-    {% else %}
-    AND block_timestamp >= getdate() - interval '9 months'
-    {% endif %}
-)
+  SELECT
+    *
+  FROM
+    {{ ref('thorchain__unstake_events') }}
+  WHERE
+    TRUE
 
-SELECT 
-  se.block_timestamp, 
-  se.block_id, 
-  se.event_id, 
-  rune_tx_id AS tx_id, 
-  'add_liquidity' AS lp_action, 
-  se.pool_name, 
-  rune_address AS from_address, 
+{% if is_incremental() %}
+AND block_timestamp >= getdate() - INTERVAL '2 days'
+{% else %}
+  AND block_timestamp >= getdate() - INTERVAL '9 months'
+{% endif %}
+)
+SELECT
+  se.block_timestamp,
+  se.block_id,
+  se.event_id,
+  rune_tx_id AS tx_id,
+  'add_liquidity' AS lp_action,
+  se.pool_name,
+  rune_address AS from_address,
   NULL AS to_address,
-  rune_e8 / POW(10, 8) AS rune_amount,
-  rune_e8 / POW(10, 8) * rune_usd AS rune_amount_usd,
-  asset_e8 / POW(10, 8) AS asset_amount,
-  asset_e8 / POW(10, 8) * asset_usd AS asset_amount_usd,
+  rune_e8 / pow(
+    10,
+    8
+  ) AS rune_amount,
+  rune_e8 / pow(
+    10,
+    8
+  ) * rune_usd AS rune_amount_usd,
+  asset_e8 / pow(
+    10,
+    8
+  ) AS asset_amount,
+  asset_e8 / pow(
+    10,
+    8
+  ) * asset_usd AS asset_amount_usd,
   stake_units,
   asset_tx_id,
   asset_address,
   asset_blockchain,
   NULL AS il_protection,
   NULL AS il_protection_usd,
-  NULl AS unstake_asymmetry,
+  NULL AS unstake_asymmetry,
   NULL AS unstake_basis_points
-FROM stakes se
-
-JOIN {{ ref('thorchain__prices') }} p 
-ON se.block_id = p.block_id AND se.pool_name = p.pool_name
-
+FROM
+  stakes se
+  LEFT JOIN {{ ref('thorchain__prices') }}
+  p
+  ON se.block_id = p.block_id
+  AND se.pool_name = p.pool_name
 UNION
-
 SELECT
-  ue.block_timestamp, 
-  ue.block_id, 
+  ue.block_timestamp,
+  ue.block_id,
   ue.event_id,
-  tx_id, 
-  'remove_liquidity' AS lp_action, 
-  ue.pool_name, 
-  from_address, 
+  tx_id,
+  'remove_liquidity' AS lp_action,
+  ue.pool_name,
+  from_address,
   to_address,
-  COALESCE(emit_rune_e8 / POW(10, 8), 0) AS rune_amount,
-  COALESCE(emit_rune_e8 / POW(10, 8) * rune_usd, 0) AS rune_amount_usd,
-  COALESCE(asset_e8 / POW(10, 8), 0) AS asset_amount,
-  COALESCE(asset_e8 / POW(10, 8) * asset_usd, 0) AS asset_amount_usd,
-  stake_units, 
-  NULL AS asset_tx_id, 
-  NULL AS asset_address, 
+  COALESCE(emit_rune_e8 / pow(10, 8), 0) AS rune_amount,
+  COALESCE(emit_rune_e8 / pow(10, 8) * rune_usd, 0) AS rune_amount_usd,
+  COALESCE(asset_e8 / pow(10, 8), 0) AS asset_amount,
+  COALESCE(asset_e8 / pow(10, 8) * asset_usd, 0) AS asset_amount_usd,
+  stake_units,
+  NULL AS asset_tx_id,
+  NULL AS asset_address,
   NULL AS asset_blockchain,
-  imp_loss_protection_e8 / POW(10, 8) AS il_protection,
-  imp_loss_protection_e8 / POW(10, 8) * rune_usd AS il_protection_usd,
-  asymmetry AS unstake_asymmetry, 
+  imp_loss_protection_e8 / pow(
+    10,
+    8
+  ) AS il_protection,
+  imp_loss_protection_e8 / pow(
+    10,
+    8
+  ) * rune_usd AS il_protection_usd,
+  asymmetry AS unstake_asymmetry,
   basis_points AS unstake_basis_points
-FROM unstakes ue
-
-JOIN {{ ref('thorchain__prices') }} p 
-ON ue.block_id = p.block_id AND ue.pool_name = p.pool_name
+FROM
+  unstakes ue
+  LEFT JOIN {{ ref('thorchain__prices') }}
+  p
+  ON ue.block_id = p.block_id
+  AND ue.pool_name = p.pool_name
