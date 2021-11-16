1af95f138b2e0f41a656ba73c035fa711369e9e5
Ava Masucci <31529186+amasucci13@users.noreply.github.com>
2021-11-16 11:28:40 -0800

    An 325&329/terra console dbt tests (#169)

---
+++ models/consoles/terra/consoles__terra_luna_staked_supplyUSD.sql
@@ -0,0 +1,14 @@
+{{ config(
+  materialized = 'view',
+  tags = ['snowflake', 'terra', 'console', 'terra_luna_staked_supplyUSD']
+) }}
+--queryId: 320abd12-4abf-4c05-a64a-a541f49e8c5c
+
+SELECT 
+date, 
+SUM(balance_usd) AS staked_supply_usd
+FROM 
+{{ ref('terra__daily_balances') }}
+WHERE currency = 'LUNA' AND balance_type = 'staked'
+GROUP BY date
+ORDER BY date DESC 
