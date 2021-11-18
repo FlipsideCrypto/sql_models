4143dcc69a8b1014fbcf0a334ceab172f5ea8161
austinFlipside <austin@flipsidecrypto.com>
2021-11-18 14:19:15 -0500

    fixing my typos

--- tests/consoles/terra/consoles_terra__terraswap_volume_dayzz-assert_no_gap.sql
+++ tests/consoles/terra/consoles_terra__terraswap_volume_dayzz-assert_no_gap.sql
@@ -1,2 +1,2 @@
-{{ date_gaps(ref("consoles__terraswap_volume"), [], "dayzz") }}
+{{ date_gaps(ref("consoles_terra__terraswap_volume"), [], "dayzz") }}
 --Several delisted currencies in output-removed contract_label logic - AM 11/12
