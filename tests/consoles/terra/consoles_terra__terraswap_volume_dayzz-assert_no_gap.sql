a6c9ae0d0550f66374ac84681051828b3defd401
austinFlipside <austin@flipsidecrypto.com>
2021-11-18 16:15:45 -0500

    missed a few comments to delete

--- tests/consoles/terra/consoles_terra__terraswap_volume_dayzz-assert_no_gap.sql
+++ tests/consoles/terra/consoles_terra__terraswap_volume_dayzz-assert_no_gap.sql
@@ -1,2 +1,1 @@
 {{ date_gaps(ref("consoles_terra__terraswap_volume"), [], "dayzz") }}
---Several delisted currencies in output-removed contract_label logic - AM 11/12
--- tests/consoles/terra/consoles_terra__transactions_per_token_metric_date-assert_no_gap.sql
+++ tests/consoles/terra/consoles_terra__transactions_per_token_metric_date-assert_no_gap.sql
@@ -1,2 +1,1 @@
 {{ date_gaps(ref("consoles_terra__transactions_per_token"), [], "metric_date") }}
---Low/missing data for SDT. Confirmed correct - AM 11/12
