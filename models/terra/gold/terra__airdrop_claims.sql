fb3084d38d621289f0a0d45be996709c6d888002
jkallogjeri <41237295+jkallogjeri@users.noreply.github.com>
2021-11-22 13:32:21 -0500

    Airdrop Claims Test fail fix

--- models/terra/gold/terra__airdrop_claims.sql
+++ models/terra/gold/terra__airdrop_claims.sql
@@ -7,34 +7,26 @@
 ) }}
 
 SELECT
-  l.blockchain,
+  m.blockchain,
   chain_id,
   block_id,
   block_timestamp,
   tx_id,
-  msg_value :execute_msg :claim :stage :: NUMBER AS airdrop_id,
+  msg_value :execute_msg :claim :stage :: STRING AS airdrop_id,
   msg_value :sender :: STRING AS claimer,
-  msg_value :execute_msg :claim :amount / pow(
-    10,
-    6
-  ) AS amount,
+  msg_value :execute_msg :claim :amount / pow(10,6) AS amount,
   msg_value :contract :: STRING AS contract_address,
   l.address AS contract_label
-FROM
-  {{ ref('silver_terra__msgs') }}
-  LEFT OUTER JOIN {{ ref('silver_crosschain__address_labels') }} AS l
-  ON msg_value :contract :: STRING = l.address AND l.blockchain = 'terra' AND l.creator = 'flipside'
-WHERE
-  msg_value :execute_msg :claim :amount IS NOT NULL
+FROM {{ ref('silver_terra__msgs') }} m
+  
+LEFT OUTER JOIN {{ ref('silver_crosschain__address_labels') }} AS l
+  ON msg_value :contract :: STRING = l.address 
+  AND l.blockchain = 'terra' 
+  AND l.creator = 'flipside'
+
+WHERE msg_value :execute_msg :claim :amount IS NOT NULL
   AND tx_status = 'SUCCEEDED'
 
 {% if is_incremental() %}
-AND block_timestamp :: DATE >= (
-  SELECT
-    MAX(
-      block_timestamp :: DATE
-    )
-  FROM
-    {{ ref('silver_terra__msgs') }}
-)
+  AND block_timestamp :: DATE >= (SELECT MAX( block_timestamp :: DATE )FROM {{ ref('silver_terra__msgs') }})
 {% endif %}
