861f51fbac40bd25c758c353ba0d2e65ebbe2db7
amasucci13 <amasucci13@gmail.com>
2021-12-03 14:32:50 -0800

    Removed token_prices_hourly_v2 file and refs

--- models/uniswapv3/eth_mainnet/uniswapv3__pool_stats.sql
+++ models/uniswapv3/eth_mainnet/uniswapv3__pool_stats.sql
@@ -5,4 +5,4 @@
   incremental_strategy = 'delete+insert',
   tags = ['snowflake', 'uniswapv3', 'pool_stats']
 ) }}
-{{ uniswapv3_pool_stats(ref('silver_uniswapv3__pool_stats'), ref('silver_uniswapv3__pools'), ref("ethereum__token_prices_hourly_v2")) }}
+{{ uniswapv3_pool_stats(ref('silver_uniswapv3__pool_stats'), ref('silver_uniswapv3__pools'), ref("ethereum__token_prices_hourly")) }}
