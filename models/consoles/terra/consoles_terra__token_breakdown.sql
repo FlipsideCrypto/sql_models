9056cfecdfffdd9889802c39baff6fdd27f877e1
amasucci13 <amasucci13@gmail.com>
2021-12-07 11:39:14 -0800

    Token_breakdown, Exchange_flows, Top_Holder_flows

---
+++ models/consoles/terra/consoles_terra__token_breakdown.sql
@@ -0,0 +1,211 @@
+{{ config(
+  materialized = 'view',
+  unique_key = "CONCAT_WS('-', metric_date, currency, segment)",
+  tags = ['snowflake', 'console', 'terra', 'token_breakdown']
+) }}
+
+
+with recent_events as 
+  (
+select *
+from  {{ ref('terra__transfers') }}
+where block_timestamp >= CURRENT_DATE - 180
+),
+
+krt_outflow as (
+
+SELECT block_timestamp::date as metric_date,
+       event_currency,
+       CASE
+           WHEN event_from_label_type = 'operator' THEN 'Operator'
+           WHEN event_from IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_from_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_from IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'krt_top_holder' and blockchain = 'terra' and start_date >= current_date - 180 ) AND event_from_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS segment,
+       sum(event_amount) AS outflow
+FROM recent_events
+WHERE event_currency = 'KRT'
+GROUP BY metric_date, event_currency, segment
+),
+
+krt_inflow as (
+  
+SELECT block_timestamp::date as metric_date,
+       event_currency,
+       CASE
+           WHEN event_to_label_type = 'operator' THEN 'Operator'
+           WHEN event_to IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_to_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_to IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'krt_top_holder' and blockchain = 'terra' and start_date >= current_date - 180 ) AND event_to_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS segment,
+       sum(event_amount) AS inflow
+FROM recent_events
+WHERE event_currency = 'KRT'
+GROUP BY metric_date, event_currency, segment
+),
+
+luna_outflow as (
+
+SELECT block_timestamp::date as metric_date,
+       event_currency,
+       CASE
+           WHEN event_from_label_type = 'operator' THEN 'Operator'
+           WHEN event_from IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_from_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_from IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'luna_top_holder' and blockchain = 'terra' and start_date >= current_date - 180 ) AND event_from_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS segment,
+       sum(event_amount) AS outflow
+FROM recent_events
+WHERE event_currency = 'LUNA'
+GROUP BY metric_date, event_currency, segment
+),
+
+luna_inflow as (
+SELECT block_timestamp::date as metric_date,
+       event_currency,
+       CASE
+           WHEN event_to_label_type = 'operator' THEN 'Operator'
+           WHEN event_to IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_to_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_to IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'luna_top_holder' and blockchain = 'terra' and start_date >= current_date - 180 ) AND event_to_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS segment,
+       sum(event_amount) AS inflow
+FROM recent_events
+WHERE event_currency = 'LUNA'
+GROUP BY metric_date, event_currency, segment
+),
+ 
+sdt_outflow as (
+
+SELECT block_timestamp::date as metric_date,
+       event_currency,
+       CASE
+           WHEN event_from_label_type = 'operator' THEN 'Operator'
+           WHEN event_from IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_from_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_from IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'sdt_top_holder' and blockchain = 'terra' and start_date >= current_date - 180 ) AND event_from_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS segment,
+       sum(event_amount) AS outflow
+FROM recent_events
+WHERE event_currency = 'SDT'
+GROUP BY metric_date, event_currency, segment
+),
+
+sdt_inflow as (
+SELECT block_timestamp::date as metric_date,
+       event_currency,
+       CASE
+           WHEN event_to_label_type = 'operator' THEN 'Operator'
+           WHEN event_to IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_to_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_to IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'sdt_top_holder' and blockchain = 'terra' and start_date >= current_date - 180 ) AND event_to_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS segment,
+       sum(event_amount) AS inflow
+FROM recent_events
+WHERE event_currency = 'SDT'
+GROUP BY metric_date, event_currency, segment
+),
+
+ust_outflow as (
+
+SELECT block_timestamp::date as metric_date,
+       event_currency,
+       CASE
+           WHEN event_from_label_type = 'operator' THEN 'Operator'
+           WHEN event_from IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_from_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_from IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'ust_top_holder' and blockchain = 'terra' and start_date >= current_date - 180 ) AND event_from_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS segment,
+       sum(event_amount) AS outflow
+FROM recent_events
+WHERE event_currency = 'UST'
+GROUP BY metric_date, event_currency, segment
+),
+
+ust_inflow as (
+SELECT block_timestamp::date as metric_date,
+       event_currency,
+       CASE
+           WHEN event_to_label_type = 'operator' THEN 'Operator'
+           WHEN event_to IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_to_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_to IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'ust_top_holder' and blockchain = 'terra' and start_date >= current_date - 180 ) AND event_to_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS segment,
+       sum(event_amount) AS inflow
+FROM recent_events
+WHERE event_currency = 'UST'
+GROUP BY metric_date, event_currency, segment
+),
+
+combine as (
+ 
+ select 
+  coalesce (i.metric_date,o.metric_date) as metric_date,
+  coalesce(i.event_currency,o.event_currency) as currency,
+  coalesce (i.segment, o.segment) as segment,
+  zeroifnull(inflow) as inflow,
+  zeroifnull(outflow) as outflow,
+  zeroifnull(inflow) - zeroifnull(outflow) as daily_change
+  from ust_inflow i
+  full outer join ust_outflow o
+  on o.metric_date = i.metric_date
+  and o.event_currency = i.event_currency
+  and o.segment = i.segment 
+
+  UNION 
+  
+  select 
+  coalesce (i.metric_date,o.metric_date) as metric_date,
+  coalesce(i.event_currency,o.event_currency) as currency,
+  coalesce (i.segment, o.segment) as segment,
+  zeroifnull(inflow) as inflow,
+  zeroifnull(outflow) as outflow,
+  zeroifnull(inflow) - zeroifnull(outflow) as daily_change
+  from sdt_inflow i
+  full outer join sdt_outflow o
+  on o.metric_date = i.metric_date
+  and o.event_currency = i.event_currency
+  and o.segment = i.segment 
+ 
+UNION 
+ 
+  select 
+  coalesce (i.metric_date,o.metric_date) as metric_date,
+  coalesce(i.event_currency,o.event_currency) as currency,
+  coalesce (i.segment, o.segment) as segment,
+  zeroifnull(inflow) as inflow,
+  zeroifnull(outflow) as outflow,
+  zeroifnull(inflow) - zeroifnull(outflow) as daily_change
+  from krt_inflow i
+  full outer join krt_outflow o
+  on o.metric_date = i.metric_date
+  and o.event_currency = i.event_currency
+  and o.segment = i.segment
+ 
+ UNION
+ 
+  select 
+  coalesce (i.metric_date,o.metric_date) as metric_date,
+  coalesce(i.event_currency,o.event_currency) as currency,
+  coalesce (i.segment, o.segment) as segment,
+  zeroifnull(inflow) as inflow,
+  zeroifnull(outflow) as outflow,
+  zeroifnull(inflow) - zeroifnull(outflow) as daily_change
+  from luna_inflow i
+  full outer join luna_outflow o
+  on o.metric_date = i.metric_date
+  and o.event_currency = i.event_currency
+  and o.segment = i.segment
+  )
+  
+  select *
+  from combine
+  order by metric_date desc
---
+++ models/consoles/terra/consoles_terra__top_holder_flows.sql
@@ -0,0 +1,151 @@
+{{ config(
+  materialized = 'view',
+  unique_key = "CONCAT_WS('-', metric_date, currency, to_segment, from_segment)",
+  tags = ['snowflake', 'console', 'terra', 'top_holder_flows']
+) }}
+
+with recent_events as 
+  (
+select *
+from {{ ref('terra__transfers') }}
+where block_timestamp >= CURRENT_DATE - 360
+
+),
+
+top_holder_flows as (
+SELECT date_trunc('day', block_timestamp) AS metric_date,
+       CASE
+           WHEN event_from_label_type = 'operator' THEN 'Operator'
+           WHEN event_from IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_from_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_from IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'krt_top_holder' and blockchain = 'terra') AND event_from_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS from_segment,
+       CASE
+           WHEN event_to_label_type = 'operator' THEN 'Operator'
+           WHEN event_to IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_to_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_to IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'krt_top_holder' and blockchain = 'terra') AND event_to_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS to_segment,
+       event_currency,
+       sum(event_amount) as volume,
+       count(distinct tx_id) AS tx_count,
+      count(distinct event_from) AS address_count
+FROM recent_events
+WHERE (from_segment = 'Top Holder' or to_segment = 'Top Holder')
+  AND from_segment != to_segment
+  AND event_currency = 'KRT'
+GROUP BY metric_date, from_segment, to_segment, event_currency
+
+  
+UNION
+
+SELECT date_trunc('day', block_timestamp) AS metric_date,
+       CASE
+           WHEN event_from_label_type = 'operator' THEN 'Operator'
+           WHEN event_from IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_from_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_from IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'sdt_top_holder' and blockchain = 'terra') AND event_from_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS from_segment,
+       CASE
+           WHEN event_to_label_type = 'operator' THEN 'Operator'
+           WHEN event_to IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_to_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_to IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'sdt_top_holder' and blockchain = 'terra') AND event_to_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS to_segment,
+       event_currency,
+       sum(event_amount) as volume,
+       count(distinct tx_id) AS tx_count,
+      count(distinct event_from) AS address_count
+FROM recent_events
+WHERE (from_segment = 'Top Holder' or to_segment = 'Top Holder')
+  AND from_segment != to_segment
+  AND event_currency = 'SDT'
+GROUP BY metric_date, from_segment, to_segment, event_currency
+
+
+UNION
+
+SELECT date_trunc('day', block_timestamp) AS metric_date,
+       CASE
+           WHEN event_from_label_type = 'operator' THEN 'Operator'
+           WHEN event_from IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_from_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_from IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'luna_top_holder' and blockchain = 'terra') AND event_from_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS from_segment,
+       CASE
+           WHEN event_to_label_type = 'operator' THEN 'Operator'
+           WHEN event_to IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_to_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_to IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'luna_top_holder' and blockchain = 'terra') AND event_to_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS to_segment,
+       event_currency,
+       sum(event_amount) as volume,
+       count(distinct tx_id) AS tx_count,
+      count(distinct event_from) AS address_count
+FROM recent_events
+WHERE (from_segment = 'Top Holder' or to_segment = 'Top Holder')
+  AND from_segment != to_segment
+  AND event_currency = 'LUNA'
+GROUP BY metric_date, from_segment, to_segment, event_currency
+
+UNION
+
+SELECT date_trunc('day', block_timestamp) AS metric_date,
+       CASE
+           WHEN event_from_label_type = 'operator' THEN 'Operator'
+           WHEN event_from IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_from_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_from IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'ust_top_holder' and blockchain = 'terra') AND event_from_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS from_segment,
+       CASE
+           WHEN event_to_label_type = 'operator' THEN 'Operator'
+           WHEN event_to IN('terra1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8pm7utl','terra1dp0taj85ruc299rkdvzp4z5pfg6z6swaed74e6') THEN 'Foundation'
+           WHEN event_to_label_type IN('distributor','cex') THEN 'Exchanges'
+           WHEN event_to IN(select distinct address from {{ ref('silver_crosschain__address_tags') }} where tag_type = 'ust_top_holder' and blockchain = 'terra') AND event_to_label_type IS NULL THEN 'Top Holder'
+           ELSE 'Smaller Wallets'
+       END AS to_segment,
+       event_currency,
+       sum(event_amount) as volume,
+       count(distinct tx_id) AS tx_count,
+      count(distinct event_from) AS address_count
+FROM recent_events
+WHERE (from_segment = 'Top Holder' or to_segment = 'Top Holder')
+  AND from_segment != to_segment
+  AND event_currency = 'UST'
+GROUP BY metric_date, from_segment, to_segment, event_currency
+  
+  )
+         
+select 
+metric_date,
+from_segment,
+to_segment,
+event_currency as currency,
+CASE
+    WHEN from_segment = 'Top Holder' THEN -(volume)
+    WHEN to_segment = 'Top Holder' THEN volume
+    END AS volume,
+CASE
+    WHEN from_segment = 'Top Holder' then tx_count
+    ELSE 0
+    END AS tx_outflow,
+CASE
+    WHEN to_segment = 'Top Holder' then tx_count
+    ELSE 0
+    END AS tx_inflow,
+CASE
+    WHEN from_segment = 'Top Holder' then address_count
+    ELSE 0
+    END AS address_outflow,
+CASE
+    WHEN to_segment = 'Top Holder' then address_count
+    ELSE 0
+    END AS address_inflow
+from top_holder_flows
