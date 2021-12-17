f287e460ab89905801f8bfe527c32119a8d1760d
jhuhnke <jessica@flipsidecrypto.com>
2021-12-17 14:33:20 -0600

    transactions - failing right now

--- models/solana/silver/silver_solana__transactions.sql
+++ models/solana/silver/silver_solana__transactions.sql
@@ -1,7 +1,62 @@
 {{ config(
   materialized = 'incremental',
-  unique_key = "block_id",
+  unique_key = "CONCAT_WS('-', block_id, tx_id)",
   incremental_strategy = 'delete+insert',
   tags = ['snowflake', 'silver_solana', 'solana_transactions']
 ) }}
+
+WITH base_tables AS (
+  SELECT  
+      *
+  FROM 
+  (
+      SELECT 
+          *, 
+          row_number() OVER (PARTITION BY block_id, tx_id ORDER BY ingested_at DESC) AS rn
+      FROM {{ source('bronze_solana', 'solana_txs') }}
+  ) sq
+  WHERE 
+    sq.rn = 1 
+)
+
+SELECT 
+    block_timestamp :: TIMESTAMP AS block_timestamp, 
+    block_id :: INTEGER AS block_id,
+    tx :transaction:message:recentBlockhash :: STRING AS recent_blockhash, 
+    tx_id :: STRING AS tx_id,
+    pre.value :mint :: STRING as pre_mint,
+    post.value :mint :: STRING as post_mint,
+    pre.value :owner :: STRING AS tx_from_address, 
+    post.value :owner :: STRING AS tx_to_address, 
+    tx :meta:fee :: INTEGER AS fee, -- This is in lamports right now
+    tx :meta:status:Err :: ARRAY AS error, -- Need some sort of coalesce statement here 
+    ins.value :programId :: STRING AS program_id, 
+    ingested_at :: TIMESTAMP AS ingested_at
+FROM base_tables, 
+    LATERAL FLATTEN (
+        input => tx :meta:preTokenBalances
+    ) pre, 
+    LATERAL FLATTEN (
+        input => tx :meta:postTokenBalances
+    ) post, 
+    LATERAL FLATTEN (
+        input => tx :transaction:message:instructions
+    ) ins
+
+WHERE 
+  1 = 1
+  
+{% if is_incremental() %}
+AND ingested_at >= (
+  SELECT
+    MAX(
+      ingested_at
+    )
+  FROM
+    {{ this }}
+)
+{% endif %}
+
+  
+
 
