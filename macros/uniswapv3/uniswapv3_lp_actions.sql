40dd61c9f96b0a193b95d2b6b8dd34f88e8de3fe
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-01-04 19:48:32 -0800

    - from xiuyang

--- macros/uniswapv3/uniswapv3_lp_actions.sql
+++ macros/uniswapv3/uniswapv3_lp_actions.sql
@@ -46,17 +46,8 @@ SELECT
   p.token1_symbol,
   prices_0.price AS token0_price,
   prices_1.price AS token1_price,
-  A.liquidity AS liquidity_raw,
-  CASE
-    WHEN token0_decimals <> 0
-    AND token1_decimals <> 0 THEN SQRT(
-      (virtual_reserves_token0 / pow(10, token0_decimals)) * virtual_reserves_token1 / pow(
-        10,
-        token1_decimals
-      )
-    )
-    ELSE 0
-  END AS liquidity_adjusted,
+  A.liquidity AS liquidity,
+  A.liquidity_adjusted,
   A.liquidity_provider,
   A.nf_position_manager_address,
   A.nf_token_id,
