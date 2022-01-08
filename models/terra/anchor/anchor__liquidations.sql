f679ca40a98d2eeae9fd6911ab28d3f2778585ab
yulike <xiuyangguan@gmail.com>
2022-01-07 21:40:41 -0500

    added the more than 3 repayment txs to the query

--- models/terra/anchor/anchor__liquidations.sql
+++ models/terra/anchor/anchor__liquidations.sql
@@ -138,26 +138,107 @@ AND block_timestamp :: DATE >= (
     {{ ref('silver_terra__msgs') }}
 )
 {% endif %}
+),
+single_payment_tbl AS (
+  SELECT DISTINCT
+    blockchain,
+    chain_id,
+    block_id,
+    block_timestamp,
+    m.tx_id,
+    bid_fee,
+    m.borrower,
+    liquidator,
+    liquidated_amount,
+    liquidated_amount_usd,
+    liquidated_currency,
+    repay_amount,
+    repay_amount_usd,
+    repay_currency,
+    contract_address,
+    contract_label
+  FROM
+    msgs m
+    JOIN events e
+    ON m.tx_id = e.tx_id
+    AND m.borrower = e.borrower 
+),
+multiple_repay_tbl_events_raw AS (
+  SELECT 
+    * 
+  FROM {{ ref('silver_terra__msg_events') }}
+  WHERE
+    event_type = 'from_contract'
+    AND tx_status = 'SUCCEEDED'
+    AND event_attributes :"0_action" :: STRING = 'liquidate_collateral'
+    AND event_attributes :"3_repay_amount" IS NOT NULL
+{% if is_incremental() %}
+AND block_timestamp :: DATE >= (
+  SELECT
+    MAX(
+      block_timestamp :: DATE
+    )
+  FROM
+    {{ ref('silver_terra__msgs') }}
 )
-SELECT DISTINCT
-  blockchain,
-  chain_id,
-  block_id,
-  block_timestamp,
-  m.tx_id,
-  bid_fee,
-  m.borrower,
-  liquidator,
-  liquidated_amount,
-  liquidated_amount_usd,
-  liquidated_currency,
-  repay_amount,
-  repay_amount_usd,
-  repay_currency,
-  contract_address,
-  contract_label
-FROM
-  msgs m
-  JOIN events e
-  ON m.tx_id = e.tx_id
-  AND m.borrower = e.borrower
+{% endif %}
+),
+multiple_repay_tbl_raw AS (
+  SELECT 
+    * 
+  FROM multiple_repay_tbl_events_raw
+  , lateral flatten ( input => event_attributes )
+  WHERE key LIKE '%_repay_amount' AND split(key, '_')[0]%2 = 1
+),
+multiple_repay_tbl AS (
+  SELECT 
+    blockchain,
+    chain_id,
+    block_id,
+    block_timestamp,
+    tx_id,
+    COALESCE(this :bid_fee / pow(
+        10,
+        6
+      ), this :"0_bid_fee" / pow(
+        10,
+        6
+      )) AS bid_fee,
+  
+    this :"0_borrower" :: STRING AS borrower,
+    COALESCE(this :liquidator :: STRING, this :"0_liquidator" :: STRING) AS liquidator,
+    COALESCE(this :collateral_amount / pow(
+        10,
+        6
+      ), this :"0_collateral_amount" / pow(
+        10,
+        6
+      )) AS liquidated_amount,
+    liquidated_amount * l.price AS liquidated_amount_usd,
+    COALESCE(this :collateral_token :: STRING, this :"0_collateral_token" :: STRING) AS liquidated_currency,
+    value / pow(
+        10,
+        6
+      ) AS repay_amount,
+    repay_amount * r.price AS repay_amount_usd,
+    COALESCE(this :stable_denom :: STRING, this :"0_stable_denom" :: STRING) AS repay_currency,
+    'terra1tmnqgvg567ypvsvk6rwsga3srp7e3lg6u0elp8' AS contract_address,
+    'Overseer' AS contract_label
+  FROM multiple_repay_tbl_raw
+  LEFT OUTER JOIN prices l
+  ON DATE_TRUNC(
+    'hour',
+    block_timestamp
+  ) = l.hour
+  AND COALESCE(event_attributes :collateral_token :: STRING, event_attributes :"0_collateral_token" :: STRING) = l.currency
+  LEFT OUTER JOIN prices r
+  ON DATE_TRUNC(
+    'hour',
+    block_timestamp
+  ) = r.hour
+  AND COALESCE(event_attributes :stable_denom :: STRING, event_attributes :"0_stable_denom" :: STRING) = r.currency
+)
+
+SELECT * FROM single_payment_tbl
+UNION ALL
+SELECT * FROM multiple_repay_tbl
