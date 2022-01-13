2c2327cb5e2866d1730ce50c6ffb72e40eddf51f
Desmond Hui <dhui@Desmonds-MacBook-Pro.local>
2022-01-12 16:04:28 -0800

    add backfilled contracts to silver table

--- models/ethereum/silver/silver_ethereum__contracts.sql
+++ models/ethereum/silver/silver_ethereum__contracts.sql
@@ -29,7 +29,19 @@ SELECT
   name
 FROM
   {{ source('ethereum','ethereum_contracts') }}
+
+union
+
+select 
+  '2000-01-01'::timestamp as system_created_at,
+  contract_address as address,
+  to_object(parse_json(contract_meta)) as meta,
+  name
+from {{ source('ethereum','ethereum_contracts_backfill') }}
+where check_json(contract_meta) is null
 )
+where address is not null
 qualify(ROW_NUMBER() over(PARTITION BY address
 ORDER BY
   system_created_at DESC)) = 1
+
