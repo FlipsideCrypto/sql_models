1ee614bf379885af8d480bc9edbd1b543fcdea63
austinFlipside <austin@flipsidecrypto.com>
2022-01-18 19:31:14 -0500

    exclude nulls

--- models/ethereum/silver/silver_ethereum__contracts.sql
+++ models/ethereum/silver/silver_ethereum__contracts.sql
@@ -6,42 +6,48 @@
   tags = ['snowflake', 'ethereum', 'silver_ethereum','silver_ethereum__contracts']
 ) }}
 
-select system_created_at,
-  address,
-  meta,
-  name
-from 
-(
 SELECT
   system_created_at,
   address,
   meta,
-  name
+  NAME
 FROM
-  {{ ref('ethereum_dbt__contracts') }}
-
-union
-
-SELECT
-  '2000-01-01'::timestamp as system_created_at,
-  address,
-  meta,
-  name
-FROM
-  {{ source('ethereum','ethereum_contracts') }}
-
-union
-
-select 
-  '2000-01-01'::timestamp as system_created_at,
-  contract_address as address,
-  to_object(parse_json(contract_meta)) as meta,
-  name
-from {{ source('ethereum','ethereum_contracts_backfill') }}
-where check_json(contract_meta) is null
-)
-where address is not null
-qualify(ROW_NUMBER() over(PARTITION BY address
+  (
+    SELECT
+      system_created_at,
+      address,
+      meta,
+      NAME
+    FROM
+      {{ ref('ethereum_dbt__contracts') }}
+    WHERE
+      meta IS NOT NULL
+    UNION
+    SELECT
+      '2000-01-01' :: TIMESTAMP AS system_created_at,
+      address,
+      meta,
+      NAME
+    FROM
+      {{ source(
+        'ethereum',
+        'ethereum_contracts'
+      ) }}
+    UNION
+    SELECT
+      '2000-01-01' :: TIMESTAMP AS system_created_at,
+      contract_address AS address,
+      TO_OBJECT(PARSE_JSON(contract_meta)) AS meta,
+      NAME
+    FROM
+      {{ source(
+        'ethereum',
+        'ethereum_contracts_backfill'
+      ) }}
+    WHERE
+      CHECK_JSON(contract_meta) IS NULL
+  )
+WHERE
+  address IS NOT NULL qualify(ROW_NUMBER() over(PARTITION BY address
 ORDER BY
   system_created_at DESC)) = 1
-
