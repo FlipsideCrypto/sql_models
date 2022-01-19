0918e87c9109f5b00471d7e6d7ec6fc4687be71c
Desmond Hui <desmond@flipsidecrypto.com>
2022-01-18 16:05:53 -0800

    adjust hardcoded start date for prices data

--- models/ethereum/ethereum__token_prices_hourly.sql
+++ models/ethereum/ethereum__token_prices_hourly.sql
@@ -8,32 +8,6 @@
   )
 }}
 
--- SELECT
---   p.symbol,
---   date_trunc('hour', recorded_at) as hour,
---   lower(a.token_address) as token_address,
---   d.decimals,
---   avg(price) as price
--- FROM
---   {{ source('shared', 'prices_v2')}} p
--- JOIN
---   {{ source('shared', 'market_asset_metadata')}} a
--- ON
---   p.asset_id = a.asset_id
--- LEFT OUTER JOIN
---   {{ source('ethereum', 'ethereum_contract_decimal_adjustments')}} d
--- ON
---   d.address = lower(a.token_address)
--- WHERE
---     (a.platform_id = '1027' OR a.asset_id = '1027' or a.platform_id = 'ethereum')
---    {% if is_incremental() %}
---      AND recorded_at >= getdate() - interval '45 days'
---    {% else %}
---      AND recorded_at >= '2020-05-05'::timestamp
---    {% endif %}
-
--- GROUP BY 1,2,3,4
-
 with hourly_prices as (
     select
       p.symbol,
@@ -48,7 +22,7 @@ with hourly_prices as (
     {% if is_incremental() %}
         and recorded_at >= current_date - 45
     {% else %}
-        and recorded_at >= '2018-02-21' -- first date with valid prices data for a symbol
+        and recorded_at >= '2020-05-05' -- first date with valid prices data
     {% endif %}  
     group by 1,2,3,4
 )
@@ -63,18 +37,18 @@ with hourly_prices as (
     {% if is_incremental() %}
       where hour >= current_date - 45
     {% else %}
-      where hour >= '2018-02-21' -- first date with valid prices data for a symbol
+      where hour >= '2020-05-05' -- first date with valid prices data
     {% endif %}  
 )
 , imputed as (
     select 
         h.hour
         , h.symbol
-        , p.price as avg_price
-        , p.token_address
+        , h.token_address
         , p.decimals
-        , last_value(p.token_address) ignore nulls over (partition by h.symbol, h.token_address order by h.hour) as lag_token_address
-        , last_value(p.decimals) ignore nulls over (partition by h.symbol, h.token_address order by h.hour) as lag_decimals
+        , p.price as avg_price
+        -- , last_value(p.token_address) ignore nulls over (partition by h.symbol, h.token_address order by h.hour) as lag_token_address
+        , lag(p.decimals) ignore nulls over (partition by h.symbol, h.token_address order by h.hour) as lag_decimals
         , lag(p.price) ignore nulls over (partition by h.symbol, h.token_address order by h.hour) as imputed_price
     from hour_symbols_pair h 
     left outer join hourly_prices p on p.hour = h.hour and p.symbol = h.symbol and p.token_address = h.token_address
@@ -82,8 +56,7 @@ with hourly_prices as (
 select 
     p.hour as hour
     , p.symbol
-    , case when token_address is not null then token_address
-      else lag_token_address end as token_address
+    , p.token_address
     , case when decimals is not null then decimals
       else lag_decimals end as decimals
     , case when avg_price is not null then avg_price
