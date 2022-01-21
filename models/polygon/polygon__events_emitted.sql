37bb9072fe629b48a2812f29ce1d40830e458ac4
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2022-01-21 13:31:45 -0600

    Update to contract name and disabled tests (#265)

--- models/polygon/polygon__events_emitted.sql
+++ models/polygon/polygon__events_emitted.sql
@@ -39,7 +39,7 @@ SELECT
   to_labels.project_name as tx_to_label,
   to_labels.address_name as tx_to_address_name,
   CONTRACT_ADDRESS AS contract_address,
-  COALESCE(contract_labels.address,CONTRACT_NAME) AS contract_name,
+  COALESCE(contract_labels.address_name, CONTRACT_NAME) AS contract_name,
   TX_SUCCEEDED AS tx_succeeded
 FROM {{ ref('silver_polygon__events_emitted')}} b
 
