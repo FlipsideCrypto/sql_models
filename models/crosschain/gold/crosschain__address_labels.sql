287114a92c8afe32bac4b2bec530db844064dc0c
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-01-27 14:24:45 -0800

    - undo typo

--- models/crosschain/gold/crosschain__address_labels.sql
+++ models/crosschain/gold/crosschain__address_labels.sql
@@ -1,17 +1,17 @@
 {{ config(
-    materialized = 'view',
-    tags = ['snowflake', 'crosschain', 'address_labels', 'gold_address_labels'],
+    materialized = 'view', 
+    tags = ['snowflake', 'crosschain', 'address_labels', 'gold_address_labels'], 
 ) }}
 
-SELECT
-    system_created_at,
-    insert_date,
-    blockchain,
-    address,
-    creator,
-    l1_label AS label_typexxx,
-    l2_label AS label_subtype,
-    address_name,
+SELECT 
+    system_created_at, 
+    insert_date, 
+    blockchain, 
+    address, 
+    creator, 
+    l1_label AS label_type, 
+    l2_label AS label_subtype, 
+    address_name, 
     project_name
-FROM
+FROM 
     {{ ref('silver_crosschain__address_labels') }}
