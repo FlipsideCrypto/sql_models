7a00dc7e513b0e61666ca4981eac8a43f5700242
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2022-01-27 14:59:41 -0600

    An 573 solana events (#270)
    * Added tables that could not be reviewed back into branch
    
    * Reduced recency tests from 3 days to 1 day
    
    * events + votes + update to transfers
    
    * Addressed comments in PR
    
    * Removed gold tests, added incrementals for instructions/inner instructions in silver_events
    
    * Added pre and post token balances to transfers - needed when token transfered is not solana
    
    * Changed ORDER BY statement in the silver events table

---
+++ models/solana/solana_dbt__instructions.sql
@@ -0,0 +1,30 @@
+{{ config(
+  materialized = 'incremental',
+  unique_key = "CONCAT_WS('-', block_id, tx_id, event_type)",
+  incremental_strategy = 'delete+insert',
+  cluster_by = ['ingested_at::DATE'],
+  tags = ['snowflake', 'solana', 'silver_solana', 'solana_events']
+) }}
+
+SELECT 
+    block_timestamp,
+    block_id, 
+    tx_id, 
+    e.index, 
+    e.value:parsed:type:: STRING AS event_type, 
+    e.value, 
+    ingested_at
+FROM {{ ref('bronze_solana__transactions') }} t,
+table(flatten(tx:transaction:message:instructions)) AS e
+WHERE e.value:parsed:type:: STRING <> 'vote'
+
+ {% if is_incremental() %}
+  AND ingested_at >= (
+    SELECT
+      MAX(
+        ingested_at
+      )
+    FROM
+      {{ this }}
+  )
+  {% endif %}
