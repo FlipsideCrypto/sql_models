12dbfece920e02278c76336003a9883be2e3ebfe
jhuhnke <jessica@flipsidecrypto.com>
2022-02-01 09:53:18 -0600

    put group by step in the silver model

--- models/solana/gold/solana__votes.sql
+++ models/solana/gold/solana__votes.sql
@@ -7,8 +7,6 @@ SELECT
     block_id, 
     block_timestamp, 
     blockchain,
-    count(block_id) AS num_votes
+    num_votes
 FROM {{ ref('silver_solana__votes') }} 
-
-GROUP BY block_id, block_timestamp, blockchain
 ORDER BY block_id DESC
--- models/solana/silver/silver_solana__votes.sql
+++ models/solana/silver/silver_solana__votes.sql
@@ -1,6 +1,6 @@
 {{ config(
   materialized = 'incremental',
-  unique_key = "CONCAT_WS('-', block_id, tx_id)",
+  unique_key = "CONCAT_WS('-', block_id)",
   incremental_strategy = 'delete+insert',
   cluster_by = ['block_timestamp::DATE'],
   tags = ['snowflake', 'solana', 'silver_solana', 'solana_votes']
@@ -8,50 +8,38 @@
 
 WITH base_table as (
   SELECT 
-    block_timestamp :: TIMESTAMP AS block_timestamp, 
     block_id :: INTEGER AS block_id,
     chain_id :: STRING AS blockchain, 
-    tx :transaction:message:recentBlockhash :: STRING AS recent_block_hash, 
-    tx_id :: STRING AS tx_id,
-    CASE WHEN tx :meta:status:Err IS NULL THEN TRUE ELSE FALSE END AS succeeded,   
-    tx :transaction:message:instructions[0]:program :: STRING AS program_type,
-    tx :transaction:message:instructions[0]:programId :: STRING AS program_id,
-    tx :transaction:message:instructions[0]:parsed:info:voteAccount :: STRING AS vote_account, 
-    tx :transaction:message:instructions[0]:parsed:info:voteAuthority :: STRING AS vote_authority, 
-    ingested_at :: TIMESTAMP AS ingested_at,
-    CASE WHEN len(tx :meta:postTokenBalances[0]) > 0 AND len(tx :meta:preTokenBalances[0]) > 0  AND SUCCEEDED = TRUE THEN TRUE ELSE FALSE END AS transfer_tx_flag
-
+    tx_id :: STRING AS tx_id, 
+    ingested_at :: TIMESTAMP AS ingested_at
   FROM {{ ref('bronze_solana__transactions') }}
 
   WHERE tx :transaction:message:instructions[0]:programId :: STRING = 'Vote111111111111111111111111111111111111111'
   
   {% if is_incremental() %}
-  AND ingested_at >= (
-    SELECT
-      MAX(
-        ingested_at
-      )
-    FROM
-      {{ this }}
-  )
-  {% endif %}
+     AND ingested_at >= getdate() - interval '2 days'
+   {% endif %}
+
+  qualify(ROW_NUMBER() over(PARTITION BY block_id :: INTEGER, tx_id :: STRING
+  ORDER BY
+  ingested_at :: TIMESTAMP DESC)) = 1
+), 
+
+v AS (
+  SELECT
+    block_id, 
+    blockchain, 
+    count(block_id) AS num_votes
+  FROM base_table
+  GROUP BY block_id, blockchain 
 )
 
-SELECT  
-  block_timestamp, 
-  block_id, 
-  blockchain, 
-  recent_block_hash, 
-  tx_id,  
-  succeeded,   
-  program_type,
-  program_id, 
-  vote_account, 
-  vote_authority,   
-  ingested_at,  
-  transfer_tx_flag
-FROM base_table
+SELECT 
+  b.block_timestamp,
+  v.block_id, 
+  v.blockchain, 
+  num_votes
+FROM v
 
-qualify(ROW_NUMBER() over(PARTITION BY block_id, tx_id
-ORDER BY
-  ingested_at DESC)) = 1
+LEFT OUTER JOIN {{ ref('silver_solana__blocks') }} b
+ON b.block_id = v.block_id
