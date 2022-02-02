f3cd643cfae5bed680acbfc546891c56b44c0ce6
jhuhnke <jessica@flipsidecrypto.com>
2022-02-02 09:53:28 -0600

    Creation of LP actions table

---
+++ models/solana/gold/solana__lp_actions.sql
@@ -0,0 +1,20 @@
+{{ config(
+    materialized = 'view', 
+    tags = ['snowflake', 'solana', 'gold_solana', 'solana_lp_actions']
+) }}
+
+SELECT 
+    block_timestamp, 
+    block_id, 
+    blockchain, 
+    recent_block_hash, 
+    tx_id, 
+    succeeded, 
+    preTokenBalances,  
+    postTokenBalances, 
+    index,
+    event_type, 
+    instruction, 
+    ingested_at
+
+FROM {{ ref('silver_solana__lp_actions') }} 
