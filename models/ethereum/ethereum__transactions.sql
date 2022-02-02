237c80264553744fc43464952cae9fb7bb9b458f
Desmond Hui <desmond@flipsidecrypto.com>
2022-02-02 10:18:11 -0800

    contracts table requires dedupe

--- models/ethereum/ethereum__transactions.sql
+++ models/ethereum/ethereum__transactions.sql
@@ -80,77 +80,83 @@ AND recorded_at >= getdate() - INTERVAL '2 days'
 GROUP BY
   p.symbol,
   HOUR
-)
-SELECT
-  t.block_timestamp,
-  t.block_id AS block_id,
-  t.tx_hash AS tx_id,
-  tx_position,
-  nonce,
-  from_address,
-  from_labels.l1_label AS from_label_type,
-  from_labels.l2_label AS from_label_subtype,
-  from_labels.project_name AS from_label,
-  from_labels.address_name AS from_address_name,
-  to_address,
-  to_labels.l1_label AS to_label_type,
-  to_labels.l2_label AS to_label_subtype,
-  to_labels.project_name AS to_label,
-  to_labels.address_name AS to_address_name,
-  COALESCE(
-    contracts.meta :symbol :: STRING,
-    contracts_old.symbol
-  ) AS symbol,
-  t.input_method AS function_signature,
-  f.text_signature AS function_name,
-  gas_price,
-  gas_limit,
-  gas_used,
-  tx_fee,
-  tx_fee * p.price AS fee_usd,
-  CASE
-    WHEN success = 1 THEN TRUE
-    ELSE FALSE
-  END AS success,
-  event_count
-FROM
-  events e
-  JOIN txn t
-  ON e.tx_hash = t.tx_hash
-  LEFT OUTER JOIN {{ source(
-    'ethereum',
-    'sha256_function_signatures'
-  ) }}
-  f
-  ON t.input_method = f.hex_signature
-  AND f.importance = 1
-  LEFT OUTER JOIN {{ source(
-    'ethereum',
-    'ethereum_token_contracts'
-  ) }}
-  contracts_old
-  ON LOWER(
-    t.to_address
-  ) = LOWER(
-    contracts_old.contract_address
-  )
-  LEFT OUTER JOIN {{ ref('silver_ethereum__contracts') }}
-  contracts
-  ON LOWER(
-    t.to_address
-  ) = LOWER(
-    contracts.address
-  )
-  LEFT OUTER JOIN eth_prices p
-  ON DATE_TRUNC(
-    'hour',
-    t.block_timestamp
-  ) = p.hour
-  LEFT OUTER JOIN {{ ref('silver_crosschain__address_labels') }} AS from_labels
-  ON from_address = from_labels.address
-  AND from_labels.blockchain = 'ethereum'
-  AND from_labels.creator = 'flipside'
-  LEFT OUTER JOIN {{ ref('silver_crosschain__address_labels') }} AS to_labels
-  ON to_address = to_labels.address
-  AND to_labels.blockchain = 'ethereum'
-  AND to_labels.creator = 'flipside'
+),
+contracts_new AS (
+  SELECT
+    LOWER(address) AS address,
+    meta :symbol :: STRING AS symbol
+  FROM
+    {{ ref('silver_ethereum__contracts') }}
+    qualify(ROW_NUMBER() over(PARTITION BY LOWER(address)
+  ORDER BY
+    NAME)) = 1)
+  SELECT
+    t.block_timestamp,
+    t.block_id AS block_id,
+    t.tx_hash AS tx_id,
+    tx_position,
+    nonce,
+    from_address,
+    from_labels.l1_label AS from_label_type,
+    from_labels.l2_label AS from_label_subtype,
+    from_labels.project_name AS from_label,
+    from_labels.address_name AS from_address_name,
+    to_address,
+    to_labels.l1_label AS to_label_type,
+    to_labels.l2_label AS to_label_subtype,
+    to_labels.project_name AS to_label,
+    to_labels.address_name AS to_address_name,
+    COALESCE(
+      contracts_new.symbol,
+      contracts_old.symbol
+    ) AS symbol,
+    t.input_method AS function_signature,
+    f.text_signature AS function_name,
+    gas_price,
+    gas_limit,
+    gas_used,
+    tx_fee,
+    tx_fee * p.price AS fee_usd,
+    CASE
+      WHEN success = 1 THEN TRUE
+      ELSE FALSE
+    END AS success,
+    event_count
+  FROM
+    events e
+    JOIN txn t
+    ON e.tx_hash = t.tx_hash
+    LEFT OUTER JOIN {{ source(
+      'ethereum',
+      'sha256_function_signatures'
+    ) }}
+    f
+    ON t.input_method = f.hex_signature
+    AND f.importance = 1
+    LEFT OUTER JOIN {{ source(
+      'ethereum',
+      'ethereum_token_contracts'
+    ) }}
+    contracts_old
+    ON LOWER(
+      t.to_address
+    ) = LOWER(
+      contracts_old.contract_address
+    )
+    LEFT OUTER JOIN contracts_new
+    ON LOWER(
+      t.to_address
+    ) = contracts_new.address
+    LEFT OUTER JOIN eth_prices p
+    ON DATE_TRUNC(
+      'hour',
+      t.block_timestamp
+    ) = p.hour
+    LEFT OUTER JOIN {{ ref('silver_crosschain__address_labels') }} AS from_labels
+    ON from_address = from_labels.address
+    AND from_labels.blockchain = 'ethereum'
+    AND from_labels.creator = 'flipside'
+    LEFT OUTER JOIN {{ ref('silver_crosschain__address_labels') }} AS to_labels
+    ON to_address = to_labels.address
+    AND to_labels.blockchain = 'ethereum'
+    AND to_labels.creator = 'flipside'
