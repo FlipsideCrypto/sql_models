27cc2e83f6863ee86d55c7734f2d2bb3432f56a4
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2022-02-09 11:45:48 -0600

    silver schema for solana transfers (#299)

--- models/solana/gold/solana__transfers.sql
+++ models/solana/gold/solana__transfers.sql
@@ -4,28 +4,21 @@
 ) }}
 
 SELECT
-    block_timestamp, 
-    block_id, 
-    blockchain, 
-    recent_block_hash, 
-    tx_id, 
-    index, 
-    event_type, 
-    preTokenBalances, 
-    postTokenBalances, 
-    instruction:parsed:info:destination :: STRING AS destination, 
-    instruction:parsed:info:source :: STRING AS source, 
-    instruction:parsed:info:authority :: STRING AS authority,
-    CASE 
-        WHEN event_type = 'transferChecked' THEN instruction:parsed:info:tokenAmount:amount/POW(10,6)
-        WHEN event_type = 'transfer' AND instruction:programId :: STRING = '11111111111111111111111111111111' THEN instruction:parsed:info:lamports/POW(10,9)
-        ELSE instruction:parsed:info:amount/POW(10,6) END
-    AS amount, 
-    instruction:programId :: STRING AS program_id, 
-    succeeded, 
-    ingested_at, 
-    CASE WHEN program_id <> 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA' THEN TRUE ELSE FALSE END AS transfer_tx_flag 
-FROM {{ ref('silver_solana__events') }}
+  block_timestamp, 
+  block_id, 
+  blockchain, 
+  recent_block_hash, 
+  tx_id, 
+  succeeded, 
+  preTokenBalances, 
+  postTokenBalances, 
+  destination, 
+  source, 
+  authority, 
+  amount, 
+  event_type, 
+  instruction, 
+  ingested_at
 
-WHERE event_type = 'transfer' 
-OR event_type = 'transferChecked'
+FROM   
+    {{ ref('silver_solana__transfers') }} 
