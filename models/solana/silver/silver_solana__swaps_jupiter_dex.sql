eefa0fbedfe953173b133c4f6507fca3dd9decf1
Desmond Hui <desmond@flipsidecrypto.com>
2022-02-14 15:31:17 -0800

    working for most scenarios

--- models/solana/silver/silver_solana__swaps_jupiter_dex.sql
+++ models/solana/silver/silver_solana__swaps_jupiter_dex.sql
@@ -12,6 +12,7 @@ WITH jupiter_dex_txs AS (
         DISTINCT i.block_id,
         i.block_timestamp,
         i.tx_id,
+        t.fee,
         t.account_keys,
         t.succeeded
     FROM
@@ -77,7 +78,7 @@ post_balances_acct_map AS (
 
 {% if is_incremental() %}
 WHERE
-    i.ingested_at >= CURRENT_DATE - 2
+    b.ingested_at >= CURRENT_DATE - 2
 {% endif %}
 ),
 pre_balances_acct_map AS (
@@ -96,7 +97,38 @@ pre_balances_acct_map AS (
 
 {% if is_incremental() %}
 WHERE
-    i.ingested_at >= CURRENT_DATE - 2
+    b.ingested_at >= CURRENT_DATE - 2
+{% endif %}
+),
+signers AS (
+    SELECT
+        t.tx_id,
+        A.value :pubkey :: STRING AS acct,
+        A.index
+    FROM
+        jupiter_dex_txs t,
+        TABLE(FLATTEN(t.account_keys)) A
+    WHERE
+        A.value :signer = TRUE
+),
+signers_info AS (
+    SELECT
+        t.tx_id,
+        t.fee,
+        s.acct,
+        b.sol_pre_balances [s.index] AS signer_sol_pre_balance,
+        b.sol_post_balances [s.index] AS signer_sol_post_balance
+    FROM
+        {{ ref('solana_dbt__sol_balances') }}
+        b
+        INNER JOIN jupiter_dex_txs t
+        ON t.tx_id = b.tx_id
+        LEFT OUTER JOIN signers s
+        ON s.tx_id = b.tx_id
+
+{% if is_incremental() %}
+WHERE
+    b.ingested_at >= CURRENT_DATE - 2
 {% endif %}
 ),
 swap_actions AS (
@@ -135,53 +167,90 @@ SELECT
     s1.block_id,
     s1.block_timestamp,
     s1.succeeded,
-    s2.owner AS swapper,
-    CASE
-        WHEN s1.succeeded = FALSE THEN 0
-        ELSE GREATEST(
-            s1.amount,
+    COALESCE(
+        s2.owner,
+        s.acct
+    ) AS swapper,
+    COALESCE(
+        GREATEST(
+            s1.amount * pow(
+                10,- s1.decimal
+            ),
             COALESCE(
-                post_from.amount - pre_from.amount,
+                pre_from.amount - post_from.amount,
                 0
+            ) * pow(
+                10,- s1.decimal
             )
+        ),
+        (
+            s.signer_sol_pre_balance - s.signer_sol_post_balance + s.fee
         ) * pow(
-            10,- s1.decimal
+            10,
+            -9
         )
-    END AS swap_from_amount,
-    s1.mint AS swap_from_mint,
-    CASE
-        WHEN s1.succeeded = FALSE THEN 0
-        ELSE GREATEST(
-            s2.amount,
+    ) AS swap_from_amount,
+    COALESCE(
+        s1.mint,
+        'So11111111111111111111111111111111111111112'
+    ) AS swap_from_mint,
+    COALESCE(
+        GREATEST(
+            s2.amount * pow(
+                10,- s2.decimal
+            ),
             COALESCE(
-                pre_to.amount - post_to.amount,
+                post_to.amount - pre_to.amount,
                 0
+            ) * pow(
+                10,- s2.decimal
             )
+        ),
+        (
+            s.signer_sol_post_balance - s.signer_sol_pre_balance + s.fee
         ) * pow(
-            10,- s2.decimal
+            10,
+            -9
         )
-    END AS swap_to_amount,
-    s2.mint AS swap_to_mint
+    ) AS swap_to_amount,
+    COALESCE(
+        s2.mint,
+        'So11111111111111111111111111111111111111112'
+    ) AS swap_to_mint
 FROM
     swaps_tmp s1
     LEFT OUTER JOIN swaps_tmp s2
     ON s1.tx_id = s2.tx_id
     AND s1.rn <> s2.rn
+    LEFT OUTER JOIN signers_info s
+    ON s.tx_id = s1.tx_id
     LEFT OUTER JOIN pre_balances_acct_map pre_from
     ON pre_from.tx_id = s1.tx_id
     AND pre_from.mint = s1.mint
-    AND pre_from.owner = s2.owner
+    AND pre_from.owner = COALESCE(
+        s2.owner,
+        s.acct
+    )
     LEFT OUTER JOIN pre_balances_acct_map pre_to
     ON pre_to.tx_id = s1.tx_id
     AND pre_to.mint = s2.mint
-    AND pre_to.owner = s2.owner
+    AND pre_to.owner = COALESCE(
+        s2.owner,
+        s.acct
+    )
     LEFT OUTER JOIN post_balances_acct_map post_from
     ON post_from.tx_id = s1.tx_id
     AND post_from.mint = s1.mint
-    AND post_from.owner = s2.owner
+    AND post_from.owner = COALESCE(
+        s2.owner,
+        s.acct
+    )
     LEFT OUTER JOIN post_balances_acct_map post_to
     ON post_to.tx_id = s1.tx_id
     AND post_to.mint = s2.mint
-    AND post_to.owner = s2.owner
+    AND post_to.owner = COALESCE(
+        s2.owner,
+        s.acct
+    )
 WHERE
     s1.rn = 1
