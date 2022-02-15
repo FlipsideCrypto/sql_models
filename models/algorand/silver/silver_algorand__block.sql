9030b3dac678762d972a0371b1ca516b79874afb
Nick <nick@flipsidecrypto.com>
2022-02-15 11:53:33 -0800

    update block & txn models to look @ hevo >2.8block

--- models/algorand/silver/silver_algorand__block.sql
+++ models/algorand/silver/silver_algorand__block.sql
@@ -5,21 +5,68 @@
   tags = ['snowflake', 'algorand', 'block', 'silver_algorand']
 ) }}
 
+WITH blockheader_fivetran AS(
+
+  SELECT
+    ROUND AS block_id,
+    realtime :: TIMESTAMP AS block_timestamp,
+    rewardslevel AS rewardslevel,
+    header :gen :: STRING AS network,
+    header :gh :: STRING AS genesis_hash,
+    header :prev :: STRING AS prev_block_hash,
+    header :txn :: STRING AS txn_root,
+    header,
+    _FIVETRAN_SYNCED
+  FROM
+    {{ source(
+      'algorand',
+      'BLOCK_HEADER'
+    ) }}
+),
+blockheader_hevo AS(
+  SELECT
+    ROUND AS block_id,
+    realtime :: TIMESTAMP AS block_timestamp,
+    rewardslevel AS rewardslevel,
+    header :gen :: STRING AS network,
+    header :gh :: STRING AS genesis_hash,
+    header :prev :: STRING AS prev_block_hash,
+    header :txn :: STRING AS txn_root,
+    header,
+    DATEADD(
+      'MS',
+      __HEVO__LOADED_AT,
+      '1970-01-01'
+    ) AS _FIVETRAN_SYNCED
+  FROM
+    {{ source(
+      'algorand_patch',
+      'BLOCK_HEADER'
+    ) }}
+),
+allBLOCKS AS(
+  SELECT
+    *
+  FROM
+    blockheader_fivetran
+  UNION
+  SELECT
+    *
+  FROM
+    blockheader_hevo
+)
 SELECT
-  ROUND AS block_id,
-  realtime :: TIMESTAMP AS block_timestamp,
-  rewardslevel AS rewardslevel,
-  header :gen :: STRING AS network,
-  header :gh :: STRING AS genesis_hash,
-  header :prev :: STRING AS prev_block_hash,
-  header :txn :: STRING AS txn_root,
+  block_id,
+  block_timestamp,
+  rewardslevel,
+  network,
+  genesis_hash,
+  prev_block_hash,
+  txn_root,
   header,
   _FIVETRAN_SYNCED
 FROM
-  {{ source(
-    'algorand',
-    'BLOCK_HEADER'
-  ) }}
+  allBLOCKS
 WHERE
   1 = 1
 
