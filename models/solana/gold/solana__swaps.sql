83b919575d19a96c48f7fc045f288953d311aa24
Desmond Hui <desmond@flipsidecrypto.com>
2022-02-14 16:06:58 -0800

    add succeeded to gold swaps output:

--- models/solana/gold/solana__swaps.sql
+++ models/solana/gold/solana__swaps.sql
@@ -1,16 +1,17 @@
 {{ config(
-    materialized = 'view', 
+    materialized = 'view',
     tags = ['snowflake', 'solana', 'gold_solana', 'solana_swaps']
 ) }}
 
-SELECT 
-    block_timestamp, 
-    block_id, 
-    tx_id, 
-    swapper, 
-    swap_from_amount,  
-    swap_from_mint, 
-    swap_to_amount, 
+SELECT
+    block_timestamp,
+    block_id,
+    tx_id,
+    succeeded,
+    swapper,
+    swap_from_amount,
+    swap_from_mint,
+    swap_to_amount,
     swap_to_mint
-
-FROM {{ ref('silver_solana__swaps_jupiter_dex') }} 
+FROM
+    {{ ref('silver_solana__swaps_jupiter_dex') }}
