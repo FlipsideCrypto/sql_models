cb7d9f68d91460e78cf3388c4d77e6b6bde70a7c
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2022-02-18 12:49:41 -0600

    switch incremental logic to cte (#335)

--- models/solana/silver/silver_solana__events.sql
+++ models/solana/silver/silver_solana__events.sql
@@ -7,6 +7,51 @@
   post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION"
 ) }}
 
+WITH base_i AS (
+  SELECT
+    block_id, 
+    tx_id, 
+    index :: INTEGER AS index, 
+    event_type, 
+    value, 
+    ingested_at
+  FROM {{ ref('solana_dbt__instructions') }} 
+
+{% if is_incremental() %}
+  WHERE ingested_at >= getdate() - interval '2 days'
+{% endif %}
+), 
+
+base_ii AS (
+  SELECT
+    block_id, 
+    tx_id, 
+    mapped_event_index :: INTEGER AS mapped_event_index, 
+    value,  
+    ingested_at
+  FROM {{ ref('solana_dbt__inner_instructions') }}
+
+{% if is_incremental() %}
+  WHERE ingested_at >= getdate() - interval '2 days'
+{% endif %}
+), 
+
+base_t AS (
+  SELECT
+    block_timestamp, 
+    block_id, 
+    tx_id, 
+    chain_id, 
+    tx, 
+    ingested_at
+
+  FROM {{ ref('bronze_solana__transactions') }}
+
+{% if is_incremental() %}
+  WHERE ingested_at >= getdate() - interval '2 days'
+{% endif %}
+)
+
 SELECT
   t.block_timestamp :: TIMESTAMP AS block_timestamp,
   t.block_id :: INTEGER AS block_id,
@@ -25,27 +70,18 @@ SELECT
   ii.value AS inner_instruction,
   t.ingested_at :: TIMESTAMP AS ingested_at
 FROM
-  {{ ref('solana_dbt__instructions') }}
+  base_i
   i
-  LEFT OUTER JOIN {{ ref('solana_dbt__inner_instructions') }}
+  LEFT OUTER JOIN base_ii
   ii
   ON ii.block_id = i.block_id
   AND ii.tx_id = i.tx_id
   AND ii.mapped_event_index = i.index
 
-{% if is_incremental() %}
-AND ii.ingested_at >= getdate() - INTERVAL '2 days'
-{% endif %}
-LEFT OUTER JOIN {{ ref('bronze_solana__transactions') }}
+LEFT OUTER JOIN base_t
 t
 ON t.block_id = i.block_id
 AND t.tx_id = i.tx_id
-
-{% if is_incremental() %}
-WHERE
-  t.ingested_at >= getdate() - INTERVAL '2 days'
-  AND i.ingested_at >= getdate() - INTERVAL '2 days'
-{% endif %}
 
 qualify(ROW_NUMBER() over(PARTITION BY t.block_id, t.tx_id, i.index
 ORDER BY
