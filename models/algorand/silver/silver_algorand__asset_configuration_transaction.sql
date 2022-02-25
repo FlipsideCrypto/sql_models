bfbf7b3057e7ba0563bf2ebab908d4a222e3b6dd
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-02-24 20:58:37 -0800

    - removed hevo bronze schema

--- models/algorand/silver/silver_algorand__asset_configuration_transaction.sql
+++ models/algorand/silver/silver_algorand__asset_configuration_transaction.sql
@@ -5,7 +5,7 @@
   tags = ['snowflake', 'algorand', 'asset_configuration', 'silver_algorand_tx']
 ) }}
 
-WITH allTXN AS (
+WITH allTXN_fivetran AS (
 
   SELECT
     ab.block_timestamp AS block_timestamp,
@@ -51,6 +51,72 @@ WITH allTXN AS (
     ON b.round = ab.block_id
   WHERE
     tx_type = 'acfg'
+),
+allTXN_hevo AS (
+  SELECT
+    ab.block_timestamp AS block_timestamp,
+    b.intra AS intra,
+    b.round AS block_id,
+    txn :txn :grp :: STRING AS tx_group_id,
+    CASE
+      WHEN b.txid IS NULL THEN ft.txn_txn_id :: text
+      ELSE b.txid :: text
+    END AS tx_id,
+    CASE
+      WHEN b.txid IS NULL THEN 'true'
+      ELSE 'false'
+    END AS inner_tx,
+    asset AS asset_id,
+    txn :txn :apar :t AS asset_supply,
+    txn :txn :snd :: text AS sender,
+    txn :txn :fee / pow(
+      10,
+      6
+    ) AS fee,
+    txn :txn :apar AS asset_parameters,
+    txn :txn :type :: STRING AS tx_type,
+    CASE
+      WHEN b.txid IS NULL THEN ft.genesis_hash :: text
+      ELSE txn :txn :gh :: STRING
+    END AS genesis_hash,
+    txn AS tx_message,
+    extra,
+    b._FIVETRAN_SYNCED
+  FROM
+    {{ source(
+      'algorand',
+      'TXN_MISSING'
+    ) }}
+    b
+    LEFT JOIN {{ ref('silver_algorand__inner_txids') }}
+    ft
+    ON b.round = ft.inner_round
+    AND b.intra = ft.inner_intra
+    LEFT JOIN {{ ref('silver_algorand__block') }}
+    ab
+    ON b.round = ab.block_id
+  WHERE
+    tx_type = 'acfg'
+    AND b.round > (
+      SELECT
+        MAX(ROUND)
+      FROM
+        {{ source(
+          'algorand',
+          'TXN'
+        ) }}
+    )
+),
+allTXN AS(
+  SELECT
+    *
+  FROM
+    allTXN_fivetran
+  UNION
+  SELECT
+    *
+  FROM
+    allTXN_hevo
 )
 SELECT
   block_timestamp,
