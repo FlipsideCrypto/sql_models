8980213f8e3d2a6abbb0651a8383b26b1117feff
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-02-25 11:07:14 -0800

    An 773/fix uncle blocks post hook (#355)
    * add post-hook to prevent uncle blocks
    
    * convert post-hook query to macro and log uncle blocks
    
    Co-authored-by: amasucci13 <amasucci13@gmail.com>

--- models/terra/silver/silver_terra__msg_events.sql
+++ models/terra/silver/silver_terra__msg_events.sql
@@ -4,7 +4,7 @@
   incremental_strategy = 'delete+insert',
   cluster_by = ['block_timestamp::DATE'],
   tags = ['snowflake', 'terra_silver', 'terra_msg_events'],
-  post_hook = 'delete from {{ this }} m using ( select distinct block_id, tx_id from {{ this }} where block_timestamp::date >= current_date - 7 qualify(rank() over (partition by tx_id order by block_id desc)) > 1 ) ub where m.block_id = ub.block_id and m.tx_id = ub.tx_id and m.block_timestamp::date >= current_date - 7;'
+  post_hook = '{{ delete_uncle_block_tx() }}'
 ) }}
 
 WITH msg_events_uncle_blocks_removed AS (
