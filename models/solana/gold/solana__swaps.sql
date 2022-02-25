550987cf7fa906a9cb6ddde6ba8a880539e0f41f
jhuhnke <jessica@flipsidecrypto.com>
2022-02-25 16:10:07 -0600

    Orca swaps silver model and union in gold

--- models/solana/gold/solana__swaps.sql
+++ models/solana/gold/solana__swaps.sql
@@ -16,3 +16,19 @@ SELECT
     swap_to_mint
 FROM
     {{ ref('silver_solana__swaps_jupiter_dex') }}
+
+UNION ALL 
+
+SELECT
+    'jupiter aggregator v2' AS swap_program,
+    block_timestamp,
+    block_id,
+    tx_id,
+    succeeded,
+    swapper,
+    swap_from_amount,
+    swap_from_mint,
+    swap_to_amount,
+    swap_to_mint
+FROM
+    {{ ref('silver_solana__swaps_orca_dex') }}
