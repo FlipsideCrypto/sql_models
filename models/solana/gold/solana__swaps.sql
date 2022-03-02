c133c9cbe77616984a40fbecb2d82cdea416eaf4
jhuhnke <jessica@flipsidecrypto.com>
2022-03-02 13:16:00 -0600

    gold swaps name change update

--- models/solana/gold/solana__swaps.sql
+++ models/solana/gold/solana__swaps.sql
@@ -4,15 +4,14 @@
 ) }}
 
 SELECT
-    'jupiter aggregator v2' AS swap_program,
     block_timestamp,
     block_id,
     tx_id,
     succeeded,
     swapper,
-    swap_from_amount,
-    swap_from_mint,
-    swap_to_amount,
-    swap_to_mint
+    from_amt AS swap_from_amount,
+    from_mint AS swap_from_mint,
+    to_amt AS swap_to_amount,
+    to_mint AS swap_to_mint
 FROM
     {{ ref('silver_solana__swaps_jupiter_dex') }}
