55dfd0a4328929b524b083277707c34067055db8
Desmond Hui <desmond@flipsidecrypto.com>
2022-03-03 11:24:39 -0800

    add orca to gold view

--- models/solana/gold/solana__swaps.sql
+++ models/solana/gold/solana__swaps.sql
@@ -16,3 +16,17 @@ SELECT
     to_mint AS swap_to_mint
 FROM
     {{ ref('silver_solana__swaps_jupiter_dex') }}
+UNION
+SELECT
+    'orca' AS swap_program,
+    block_timestamp,
+    block_id,
+    tx_id,
+    succeeded,
+    swapper,
+    from_amt,
+    from_mint,
+    to_amt,
+    to_mint
+FROM
+    {{ ref('silver_solana__swaps_orca_dex') }}
