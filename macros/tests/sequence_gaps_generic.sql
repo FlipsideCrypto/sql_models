7855f086d341d8f95929566861a938c3c2cdd298
austinFlipside <austin@flipsidecrypto.com>
2022-03-09 12:56:54 -0500

    remove generic tests

---
+++ macros/tests/sequence_gaps_generic.sql
@@ -0,0 +1,34 @@
+{% test sequence_gaps_generic(
+    model,
+    partition_by,
+    column_name
+) %}
+{%- set partition_sql = partition_by | join(", ") -%}
+{%- set previous_column = "prev_" ~ column_name -%}
+WITH source AS (
+    SELECT
+        {{ partition_sql + "," if partition_sql }}
+        {{ column_name }},
+        LAG(
+            {{ column_name }},
+            1
+        ) over (
+            {{ "PARTITION BY " ~ partition_sql if partition_sql }}
+            ORDER BY
+                {{ column_name }} ASC
+        ) AS {{ previous_column }}
+    FROM
+        {{ model }}
+)
+SELECT
+    {{ partition_sql + "," if partition_sql }}
+    {{ previous_column }},
+    {{ column_name }},
+    {{ column_name }} - {{ previous_column }}
+    - 1 AS gap
+FROM
+    source
+WHERE
+    {{ column_name }} - {{ previous_column }} <> 1
+ORDER BY
+    gap DESC {% endtest %}
---
+++ tests/ethereum/ethereum__token_prices_hourly-date-gaps.sql
@@ -0,0 +1,1 @@
+{{ date_gaps(ref("ethereum__token_prices_hourly"), [], "HOUR") }}
