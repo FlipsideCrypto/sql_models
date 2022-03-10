1b3bb8d93896fcb5918ef1ab2d35bad894e2def9
Desmond Hui <desmond@flipsidecrypto.com>
2022-03-10 09:34:34 -0800

    temporarily filter out backfill data since it has issues

--- models/solana/bronze/bronze_solana__transactions.sql
+++ models/solana/bronze/bronze_solana__transactions.sql
@@ -1,21 +1,23 @@
 {{ config (
-    materialized = 'view', 
-    tags = ['snowflake', 'solana', 'bronze_solana', 'solana_transactions']
+  materialized = 'view',
+  tags = ['snowflake', 'solana', 'bronze_solana', 'solana_transactions']
 ) }}
 
 SELECT
-   record_id, 
-   tx_id, 
-   tx_block_index, 
-   offset_id AS block_id, 
-   block_id AS offset_id, 
-   block_timestamp, 
-   network, 
-   chain_id, 
-   tx, 
-   ingested_at
-FROM 
-    {{ source(
-      'prod',
-      'solana_txs'
-    ) }} 
+  record_id,
+  tx_id,
+  tx_block_index,
+  offset_id AS block_id,
+  block_id AS offset_id,
+  block_timestamp,
+  network,
+  chain_id,
+  tx,
+  ingested_at
+FROM
+  {{ source(
+    'prod',
+    'solana_txs'
+  ) }}
+WHERE
+  offset_id >= 110547628
