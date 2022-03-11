54627eeb1ffd34595a1f11630fba8c9a12a2de87
Nick <nick@flipsidecrypto.com>
2022-03-10 22:51:49 -0500

    gold swaps model

---
+++ models/algorand/gold/algorand_swaps.sql
@@ -0,0 +1,58 @@
+{{ config(
+    materialized = 'view',
+    tags = ['snowflake', 'algorand_views', 'algorand_swaps', 'gold'],
+) }}
+
+SELECT
+    'tinyman' AS swap_program,
+    pa.block_timestamp,
+    block_id,
+    intra,
+    tx_group_id,
+    app_id,
+    swapper,
+    from_asset_name,
+    swap_from_asset_id,
+    swap_from_amount,
+    pool_address,
+    to_asset_name,
+    swap_to_asset_id,
+    swap_to_amount
+FROM
+    {{ ref('silver_algorand__swaps_tinyman_dex') }}
+UNION
+SELECT
+    'algofi' AS swap_program,
+    pa.block_timestamp,
+    block_id,
+    intra,
+    tx_group_id,
+    app_id,
+    swapper,
+    from_asset_name,
+    swap_from_asset_id,
+    swap_from_amount,
+    pool_address,
+    to_asset_name,
+    swap_to_asset_id,
+    swap_to_amount
+FROM
+    {{ ref('silver_algorand__swaps_algofi_dex') }}
+UNION
+SELECT
+    'pactfi' AS swap_program,
+    pa.block_timestamp,
+    block_id,
+    intra,
+    tx_group_id,
+    app_id,
+    swapper,
+    from_asset_name,
+    swap_from_asset_id,
+    swap_from_amount,
+    pool_address,
+    to_asset_name,
+    swap_to_asset_id,
+    swap_to_amount
+FROM
+    {{ ref('silver_algorand__swaps_pactfi_dex') }}
