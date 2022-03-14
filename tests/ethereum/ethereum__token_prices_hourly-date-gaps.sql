fd0ded7c66dd60e312f4783ebec2a1ae96734872
austinFlipside <austin@flipsidecrypto.com>
2022-03-14 09:17:55 -0400

    updates

--- tests/ethereum/ethereum__token_prices_hourly-date-gaps.sql
+++ tests/ethereum/ethereum__token_prices_hourly-date-gaps.sql
@@ -1,1 +1,1 @@
-{{ date_gaps(ref("ethereum__token_prices_hourly"), [], "HOUR") }}
+{{ date_gaps(ref("ethereum__token_prices_hourly"), [], "HOUR::date") }}
---
+++ tests/ethereum/ethereum__udm_events-date-gaps.sql
@@ -0,0 +1,1 @@
+{{ date_gaps(ref("ethereum__udm_events"), [], "BLOCK_TIMESTAMP") }}
