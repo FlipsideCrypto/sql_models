43342b02e338a7e50b8593095572c98349b5834b
jkallogjeri <41237295+jkallogjeri@users.noreply.github.com>
2022-03-15 19:37:14 -0400

    Changed from view to table & added incre logic

--- models/consoles/terra/consoles_terra__total_users.sql
+++ models/consoles/terra/consoles_terra__total_users.sql
@@ -1,6 +1,8 @@
 {{ config(
-  materialized = 'view',
+  materialized = 'table',
   unique_key = 'date',
+  incremental_strategy = 'delete+insert',
+  cluster_by = ['date'],
   tags = ['snowflake', 'terra', 'console', 'terra_total_users']
 ) }}
 
@@ -9,4 +11,9 @@ SELECT
   COUNT(DISTINCT address) as total_users
 FROM {{ ref('terra__daily_balances') }}
 WHERE date >= CURRENT_DATE - 30
+
+{% if is_incremental() %}
+  AND block_timestamp :: DATE >= (SELECT MAX( block_timestamp :: DATE )FROM {{ ref('silver_terra__msgs') }})
+{% endif %}
+
 GROUP BY 1
