3fa09bd4858b9208738e2f3ef7a783596cfdc968
Ava Masucci <31529186+amasucci13@users.noreply.github.com>
2022-04-04 18:31:26 -0700

    An 595/terra undecoded token contracts (#417)

--- models/terra/silver/silver_terra__undecoded_wormhole_contracts.sql
+++ models/terra/silver/silver_terra__undecoded_wormhole_contracts.sql
@@ -2,6 +2,7 @@
   materialized = 'incremental',
   unique_key = "token_contract",
   incremental_strategy = 'merge',
+  tags = ['snowflake', 'terra', 'undecoded', 'terra_contracts']
 ) }}
 
 SELECT
@@ -13,3 +14,21 @@ FROM
 WHERE
   event_attributes :creator = 'terra10nmmwe8r3g99a9newtqa7a75xfgs2e8z87r2sf' --wormhole contracts
   AND block_timestamp > '2021-06-01'
+  AND token_contract NOT IN (
+    SELECT
+      address
+    FROM
+      {{ ref('silver_terra__contract_info') }}
+  )
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= COALESCE(
+  (
+    SELECT
+      MAX(_inserted_timestamp)
+    FROM
+      {{ this }}
+  ),
+  '1900-01-01'
+)
+{% endif %}
