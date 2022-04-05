fa9ff152eb2bbecdb60d60e4bf1aaed820200697
chiesel <100894230+chiesel@users.noreply.github.com>
2022-04-05 08:53:44 -1000

    updated the silver table

--- models/terra/silver/silver_terra__event_actions.sql
+++ models/terra/silver/silver_terra__event_actions.sql
@@ -11,26 +11,21 @@ WITH raw_table AS (
   SELECT 
     *
   FROM {{ ref('terra_dbt__msg_events_actions') }} 
-  {% if is_incremental() %}
-  WHERE
-    _inserted_timestamp >= (
-      SELECT
-        MAX(_inserted_timestamp)
-      FROM
-        {{ this }}
-    )
-  {% endif %}
+),
+
+clean_table AS (
+  SELECT DISTINCT
+    blockchain, 
+    block_id, 
+    block_timestamp, 
+    chain_id, 
+    tx_id, 
+    SPLIT(key, '_')[0]::INTEGER AS action_index, 
+    value:contract_address::STRING AS action_contract_address, 
+    COALESCE(value:action_log:action::STRING, value:action_log:method::STRING) AS action_method, 
+    object_delete(value:action_log, 'action', 'method') AS action_log
+  FROM raw_table r,
+  lateral flatten(input => r.event_attributes_actions)
 )
 
-SELECT DISTINCT
-  blockchain, 
-  block_id, 
-  block_timestamp, 
-  chain_id, 
-  tx_id, 
-  SPLIT(key, '_')[0]::INTEGER AS action_index, 
-  value:contract_address::STRING AS action_contract_address, 
-  COALESCE(value:action_log:action::STRING, value:action_log:method::STRING) AS action_method, 
-  object_delete(value:action_log, 'action', 'method') AS action_log
-FROM raw_table r,
-lateral flatten(input => r.event_attributes_actions)
+SELECT * FROM clean_table
