65f303cc81baf903ddaf6560e26c893e220b0f6e
Desmond Hui <desmond@flipsidecrypto.com>
2022-04-06 14:52:47 -0700

    modify existing test to use configurable delay

--- tests/algorand/silver/silver_algorand__block__block_id-assert_no_gap.sql
+++ tests/algorand/silver/silver_algorand__block__block_id-assert_no_gap.sql
@@ -1,1 +1,1 @@
-{{ delayed_sequence_gaps(ref('silver_algorand__block'), [], "block_id", "_inserted_timestamp") }}
+{{ delayed_sequence_gaps(ref('silver_algorand__block'), [], "block_id", "_inserted_timestamp", "15 HOURS") }}
--- tests/algorand/silver/silver_algorand__transactions__intra-assert_no_gap.sql
+++ tests/algorand/silver/silver_algorand__transactions__intra-assert_no_gap.sql
@@ -1,1 +1,1 @@
-{{ delayed_sequence_gaps(ref('silver_algorand__transactions'), ["block_id","tx_id"], "intra", "_inserted_timestamp") }}
+{{ delayed_sequence_gaps(ref('silver_algorand__transactions'), ["block_id","tx_id"], "intra", "_inserted_timestamp", "15 HOURS") }}
