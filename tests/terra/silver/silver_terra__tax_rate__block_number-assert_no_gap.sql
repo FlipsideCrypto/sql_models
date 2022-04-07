a3a89bf7a30703677aae6b56c57ad84e0b35f92b
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-04-07 08:53:37 -0700

    Feature/delay seq gap test period configurable (#458)
    * make delayed period configurable
    
    * modify existing test to use configurable delay
    
    * use delayed gap test for some terra models
    
    * remove unnecessary order by
    
    * seq gap test to alert only if gap before some timestamp

--- tests/terra/silver/silver_terra__tax_rate__block_number-assert_no_gap.sql
+++ tests/terra/silver/silver_terra__tax_rate__block_number-assert_no_gap.sql
@@ -1,1 +1,1 @@
-{{ delayed_sequence_gaps(ref('silver_terra__tax_rate'), [], "block_number", "_inserted_timestamp", "2 HOURS") }}
+{{ sequence_gaps_buffered_look_back(ref('silver_terra__tax_rate'), [], "block_number", "_inserted_timestamp", "2 HOURS") }}
