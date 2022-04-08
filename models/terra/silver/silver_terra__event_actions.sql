20f137ec36251c35f6d86daa518b4bb9db35e17b
yulike <xiuyangguan@gmail.com>
2022-04-08 15:43:45 -0400

    Update silver_terra__event_actions.sql

--- models/terra/silver/silver_terra__event_actions.sql
+++ models/terra/silver/silver_terra__event_actions.sql
@@ -1,6 +1,6 @@
 {{ config(
   materialized = 'incremental',
-  unique_key = "CONCAT_WS('-', tx_id, action_index)",
+  unique_key = "CONCAT_WS('-', tx_id, action_index, msg_index)",
   incremental_strategy = 'delete+insert',
   cluster_by = ['block_timestamp::DATE'],
   tags = ['snowflake', 'terra_silver', 'event_actions']
