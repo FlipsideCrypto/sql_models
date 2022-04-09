1cdd9e5c06421531c48a3c44932bedd3b99da5cf
austinFlipside <austin@flipsidecrypto.com>
2022-04-08 20:57:39 -0400

    fix nft models

--- models/ethereum/nft/ethereum__nft_events.sql
+++ models/ethereum/nft/ethereum__nft_events.sql
@@ -296,13 +296,6 @@ AND HOUR >= getdate() - INTERVAL '5 days'
 )
 WHERE
   rn = 1
-),
-decimals AS (
-  SELECT
-    LOWER(address) AS address,
-    meta :decimals :: INTEGER AS decimals
-  FROM
-    {{ ref('ethereum__contracts') }}
 )
 SELECT
   nft.event_platform,
@@ -319,10 +312,7 @@ SELECT
   nft.event_from :: STRING AS event_from,
   nft.event_to :: STRING AS event_to,
   nft.price,
-  CASE
-    WHEN decimals IS NULL THEN NULL
-    ELSE nft.price * p.price
-  END AS price_usd,
+  nft.price * p.price AS price_usd,
   nft.platform_fee,
   nft.creator_fee,
   nft.tx_currency
@@ -340,5 +330,3 @@ FROM
   ON nft.contract_address = contract_labels.address
   AND contract_labels.blockchain = 'ethereum'
   AND contract_labels.creator = 'flipside'
-  LEFT JOIN decimals
-  ON nft.contract_address = decimals.address
