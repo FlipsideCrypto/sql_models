9bd456281e14a0f69a7fdb9cde8466db147ed117
flipside-kellen <kellen@flipsidecrypto.com>
2022-04-11 11:24:32 -0700

    _USD column updates

--- models/thorchain/thorchain__pool_block_balances.sql
+++ models/thorchain/thorchain__pool_block_balances.sql
@@ -11,11 +11,11 @@ SELECT
   bpd.block_id,
   bpd.pool_name,
   COALESCE(rune_e8 / pow(10, 8), 0) AS rune_amount,
-  COALESCE(rune_e8 / pow(10, 8) * rune_usd, 0) AS rune_amount_usd,
+  COALESCE(rune_e8 / pow(10, 8), 0) * rune_usd AS rune_amount_usd,
   COALESCE(asset_e8 / pow(10, 8), 0) AS asset_amount,
-  COALESCE(asset_e8 / pow(10, 8) * asset_usd, 0) AS asset_amount_usd,
+  COALESCE(asset_e8 / pow(10, 8), 0) * asset_usd AS asset_amount_usd,
   COALESCE(synth_e8 / pow(10, 8), 0) AS synth_amount,
-  COALESCE(synth_e8 / pow(10, 8) * asset_usd, 0) AS synth_amount_usd
+  COALESCE(synth_e8 / pow(10, 8), 0) * asset_usd AS synth_amount_usd
 FROM
   {{ ref('thorchain__block_pool_depths') }}
   bpd
