076e465d2ae5d4956c4e4f5599634e9fee932e56
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-04-11 14:49:42 -0700

    - add environment specific routes for function creation - use run_query macros so macros can be run as operations

--- macros/terra/all_udfs.sql
+++ macros/terra/all_udfs.sql
@@ -1,4 +1,12 @@
 {% macro udf_bulk_get_contract_info() %}
+    {% set sql %}
     CREATE
-    OR REPLACE EXTERNAL FUNCTION silver_terra.udf_bulk_get_contract_info() returns text api_integration = analytics_serverless_api AS 'https://knloth5ct4.execute-api.us-east-1.amazonaws.com/prod/bulk_get_contract_info'
+    OR REPLACE EXTERNAL FUNCTION silver_terra.udf_bulk_get_contract_info() returns text api_integration = analytics_serverless_api AS {% if target.name == "prod" -%}
+        'https://knloth5ct4.execute-api.us-east-1.amazonaws.com/prod/bulk_get_contract_info'
+    {% else %}
+        'https://90jvvtja5m.execute-api.us-east-1.amazonaws.com/dev/bulk_get_contract_info'
+    {%- endif %}
+
+    {% endset %}
+    {% do run_query(sql) %}
 {% endmacro %}
