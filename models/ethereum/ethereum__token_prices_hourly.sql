d46ed8755c4a165c0728921a85448452dc9967e0
Desmond Hui <desmond@flipsidecrypto.com>
2022-04-12 14:51:11 -0700

    make records single symbol per address

--- models/ethereum/ethereum__token_prices_hourly.sql
+++ models/ethereum/ethereum__token_prices_hourly.sql
@@ -10,7 +10,8 @@ WITH full_decimals AS (
 
   SELECT
     LOWER(address) AS contract_address,
-    meta :decimals :: INT AS decimals
+    meta :decimals :: INT AS decimals,
+    meta :symbol :: STRING AS symbol
   FROM
     {{ ref('silver_ethereum__contracts') }}
   WHERE
@@ -150,10 +151,10 @@ FINAL AS (
   SELECT
     p.hour AS HOUR,
     p.token_address,
-    CASE
-      WHEN symbol IS NOT NULL THEN symbol
-      ELSE lag_symbol
-    END AS symbol,
+    -- CASE
+    --   WHEN symbol IS NOT NULL THEN symbol
+    --   ELSE lag_symbol
+    -- END AS symbol,
     CASE
       WHEN decimals IS NOT NULL THEN decimals
       ELSE lag_decimals
@@ -172,13 +173,15 @@ FINAL AS (
     price IS NOT NULL
 )
 SELECT
-  HOUR,
-  token_address,
-  symbol,
-  decimals,
-  price,
-  is_imputed
+  f.HOUR,
+  f.token_address,
+  d.symbol,
+  f.decimals,
+  f.price,
+  f.is_imputed
 FROM
-  FINAL qualify(ROW_NUMBER() over(PARTITION BY HOUR, token_address, symbol
+  FINAL f
+left outer join full_decimals d on d.contract_address = f.token_address
+qualify(ROW_NUMBER() over(PARTITION BY f.HOUR, f.token_address, d.symbol
 ORDER BY
-  decimals DESC) = 1)
+  f.decimals DESC) = 1)
