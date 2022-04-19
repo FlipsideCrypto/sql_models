5ebd1e91e7fec56a31c256521d9ecdb368a301d3
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-04-19 10:32:48 -0700

    - add changes to dev udf

--- macros/terra/all_udfs.sql
+++ macros/terra/all_udfs.sql
@@ -3,6 +3,6 @@
     OR REPLACE EXTERNAL FUNCTION silver_terra.udf_bulk_get_contract_info() returns text api_integration = aws_terra_api AS {% if target.name == "prod" -%}
         'https://knloth5ct4.execute-api.us-east-1.amazonaws.com/prod/bulk_get_contract_info'
     {% else %}
-        'https://90jvvtja5m.execute-api.us-east-1.amazonaws.com/dev/bulk_get_contract_info'
+        'https://3wcc5lj48d.execute-api.us-east-1.amazonaws.com/dev/bulk_get_contract_info'
     {%- endif %}
 {% endmacro %}
