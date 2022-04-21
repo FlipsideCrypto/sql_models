ff51297d0978ca7caf4a7b4c2bd6b0b550096400
Nick <nick@flipsidecrypto.com>
2022-04-21 14:19:09 -0400

    add pool addresses model and union to

--- models/algorand/gold/algorand__labels.sql
+++ models/algorand/gold/algorand__labels.sql
@@ -1,16 +1,28 @@
 {{ config(
-      materialized='view',
-      tags=['snowflake', 'algorand_views', 'labels', 'algorand_labels', 'address_labels']  
-    ) 
-}}
+  materialized = 'view',
+  tags = ['snowflake', 'algorand_views', 'labels', 'algorand_labels', 'address_labels']
+) }}
 
 SELECT
-  blockchain, 
-  creator, 
+  blockchain,
+  creator,
   address,
-  l1_label as label_type,
-  l2_label as label_subtype,
-  project_name as label, 
-  address_name as address_name
-FROM {{ref('silver_crosschain__address_labels')}}
-WHERE blockchain = 'algorand'
+  l1_label AS label_type,
+  l2_label AS label_subtype,
+  project_name AS label,
+  address_name AS address_name
+FROM
+  {{ ref('silver_crosschain__address_labels') }}
+WHERE
+  blockchain = 'algorand'
+UNION
+SELECT
+  blockchain,
+  creator,
+  address,
+  label_type,
+  label_subtype,
+  label,
+  address_name
+FROM
+  {{ ref('silver_algorand__pool_addresses') }}
