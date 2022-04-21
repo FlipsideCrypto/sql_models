0235faf5a460f8661f229b8ffa65b8d49608606a
Eric Laurello <eric.laurello@flipsidecrypto.com>
2022-04-21 11:33:48 -0400

    Shared branch for new objects

---
+++ models/algorand/silver/silver_algorand__all_unpulled_txn_rewards.sql
@@ -0,0 +1,19 @@
+{{ config(
+  materialized = 'view'
+) }}
+
+SELECT
+  sender AS account,
+  tx_id
+FROM
+  {{ ref("silver_algorand__transactions") }}
+  -- EXCEPT
+  -- SELECT
+  --   address,
+  --   tx_id
+  -- FROM
+  --   {{ ref("silver_algorand__transactions") }}
+ORDER BY
+  block_timestamp DESC
+LIMIT
+  1000000
