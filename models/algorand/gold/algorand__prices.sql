4b7fe69a0f11ed873f188e951bf6a114c8896e15
Eric Laurello <eric.laurello@flipsidecrypto.com>
2022-04-25 13:27:02 -0400

    gold view

---
+++ models/algorand/gold/algorand__prices.sql
@@ -0,0 +1,18 @@
+{{ config(
+    materialized = 'view',
+    tags = ['snowflake', 'algorand_views', 'algorand_swaps', 'algorand_prices', 'gold'],
+) }}
+
+SELECT
+    block_hour,
+    asset_id,
+    asset_name,
+    price,
+    min_price_usd_hour,
+    max_price_usd_hour,
+    volatility_measure,
+    swaps_in_hour,
+    volume_in_hour,
+    price_source
+FROM
+    {{ ref('silver_algorand__prices') }}
