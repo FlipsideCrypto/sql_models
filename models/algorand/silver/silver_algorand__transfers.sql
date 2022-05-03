69c5f431aad4f30306ab31bb2c8d0a1b0c492a6c
Nick <nick@flipsidecrypto.com>
2022-05-03 17:45:33 -0400

    adding algorand transfers model

---
+++ models/algorand/silver/silver_algorand__transfers.sql
@@ -0,0 +1,100 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = '_unique_key',
+    incremental_strategy = 'merge',
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['snowflake', 'algorand', 'payment','transfer', 'silver_algorand_tx']
+) }}
+
+SELECT
+    block_timestamp,
+    intra,
+    block_id,
+    tx_group_id,
+    tx_id,
+    inner_tx,
+    sender AS tx_sender,
+    sender AS asset_sender,
+    receiver,
+    0 AS asset_id,
+    amount,
+    fee,
+    tx_type,
+    tx_type_name,
+    genesis_hash,
+    tx_message,
+    extra,
+    concat_ws(
+        '-',
+        block_id :: STRING,
+        intra :: STRING
+    ) AS _unique_key,
+    _INSERTED_TIMESTAMP
+FROM
+    {{ ref('silver_algorand__payment_transaction') }}
+WHERE
+    1 = 1
+
+{% if is_incremental() %}
+AND _INSERTED_TIMESTAMP >= (
+    SELECT
+        MAX(
+            _INSERTED_TIMESTAMP
+        )
+    FROM
+        {{ this }}
+) - INTERVAL '4 HOURS'
+{% endif %}
+UNION
+SELECT
+    block_timestamp,
+    intra,
+    block_id,
+    tx_group_id,
+    tx_id,
+    inner_tx,
+    sender AS tx_sender,
+    IFNULL(
+        asset_sender,
+        sender
+    ) AS asset_sender,
+    asset_receiver AS receiver,
+    assetTransfer.asset_id AS asset_id,
+    CASE
+        WHEN asset.decimals > 0 THEN asset_amount :: FLOAT / pow(
+            10,
+            asset.decimals
+        )
+        ELSE asset_amount :: FLOAT
+    END AS amount,
+    fee,
+    tx_type,
+    tx_type_name,
+    genesis_hash,
+    tx_message,
+    extra,
+    concat_ws(
+        '-',
+        assetTransfer.block_id :: STRING,
+        assetTransfer.intra :: STRING
+    ) AS _unique_key,
+    assetTransfer._INSERTED_TIMESTAMP
+FROM
+    {{ ref('silver_algorand__asset_transfer_transaction') }}
+    assetTransfer
+    LEFT JOIN {{ ref('silver_algorand__asset') }}
+    asset
+    ON assetTransfer.asset_transferred = asset.asset_id
+WHERE
+    1 = 1
+
+{% if is_incremental() %}
+AND assetTransfer._INSERTED_TIMESTAMP >= (
+    SELECT
+        MAX(
+            _INSERTED_TIMESTAMP
+        )
+    FROM
+        {{ this }}
+) - INTERVAL '4 HOURS'
+{% endif %}
