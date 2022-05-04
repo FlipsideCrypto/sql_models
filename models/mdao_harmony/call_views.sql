461a226890dc8fb847ca783fa252f265a80487ea
jhuhnke <jessica@flipsidecrypto.com>
2022-05-04 12:45:05 -0500

    call views mods

--- models/mdao_harmony/call_views.sql
+++ models/mdao_harmony/call_views.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE SCHEMA FLIPSIDE_DEV_DB.MDAO_HARMONY;
 USE DATABASE FLIPSIDE_DEV_DB; 
 USE SCHEMA MDAO_HARMONY; 
 
-SET table_names = (
+VAR table_names = (
    SELECT 
         LISTAGG(TABLE_NAME, ',') 
    FROM ref({'mdao_harmony__information_schema__tables'})
