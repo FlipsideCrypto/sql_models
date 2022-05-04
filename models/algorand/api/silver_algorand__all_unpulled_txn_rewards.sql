06d06fb933678f7f6bf7a3a4ebd53a65ba44905e
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-05-04 11:45:25 -0700

    - add external table package - add bronze source for algorand api

--- models/algorand/api/silver_algorand__all_unpulled_txn_rewards.sql
+++ models/algorand/api/silver_algorand__all_unpulled_txn_rewards.sql
@@ -7,13 +7,17 @@ SELECT
   tx_id AS txid
 FROM
   {{ ref("silver_algorand__transactions") }}
-  -- EXCEPT
-  -- SELECT
-  --   address,
-  --   tx_id
-  -- FROM
-  --   {{ ref("silver_algorand__transactions") }}
+  {# EXCEPT
+SELECT
+  address,
+  tx_id
+FROM
+  {{ source(
+    "bronze",
+    "algorand_api"
+  ) }}
+  #}
 ORDER BY
   block_timestamp DESC
 LIMIT
-  1000000
+  10000
