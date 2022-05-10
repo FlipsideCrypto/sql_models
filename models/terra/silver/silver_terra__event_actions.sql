81177c39366c3943c427091f4d8265c3fe0af8fe
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-05-10 15:39:33 -0700

    - reverting to main

--- models/terra/silver/silver_terra__event_actions.sql
+++ models/terra/silver/silver_terra__event_actions.sql
@@ -27,32 +27,23 @@ qualify(RANK() over(PARTITION BY tx_id
 ORDER BY
   block_id DESC)) = 1
 )
-SELECT
-  _inserted_timestamp,
-  blockchain,
-  block_id,
-  block_timestamp,
-  chain_id,
-  tx_id,
-  SPLIT(
-    key,
-    '_'
-  ) [0] :: INTEGER AS action_index,
-  msg_index,
-  VALUE :contract_address :: STRING AS action_contract_address,
-  COALESCE(
-    VALUE :action_log :action :: STRING,
-    VALUE :action_log :method :: STRING
-  ) AS action_method,
-  OBJECT_DELETE(
-    VALUE :action_log,
-    'action',
-    'method'
-  ) AS action_log
-FROM
-  event_actions_uncle_blocks_removed r,
-  LATERAL FLATTEN(
-    input => r.event_attributes_actions
-  ) qualify(ROW_NUMBER() over(PARTITION BY chain_id, block_id, tx_id, msg_index, action_index, action_contract_address, action_method
+
+  SELECT
+    _inserted_timestamp,
+    blockchain, 
+    block_id, 
+    block_timestamp, 
+    chain_id, 
+    tx_id, 
+    SPLIT(key, '_')[0]::INTEGER AS action_index, 
+    msg_index,
+    value:contract_address::STRING AS action_contract_address, 
+    COALESCE(value:action_log:action::STRING, value:action_log:method::STRING) AS action_method, 
+    object_delete(value:action_log, 'action', 'method') AS action_log
+  FROM event_actions_uncle_blocks_removed r,
+  lateral flatten(input => r.event_attributes_actions)
+
+qualify(ROW_NUMBER() over(PARTITION BY chain_id, block_id, tx_id, msg_index, action_index, action_contract_address, action_method
 ORDER BY
   system_created_at DESC)) = 1
+
