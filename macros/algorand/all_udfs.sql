491caf4723058898ce888bc7aa7655411f5fa666
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-05-10 18:15:34 -0700

    - fix route for silver_algorand.udf_bulk_get_tx

--- macros/algorand/all_udfs.sql
+++ macros/algorand/all_udfs.sql
@@ -1,8 +1,8 @@
 {% macro create_algorand_udf_bulk_get_tx_info() %}
     CREATE
     OR REPLACE EXTERNAL FUNCTION silver_algorand.udf_bulk_get_tx() returns text api_integration = aws_algorand_api AS {% if target.name == "prod" -%}
-        'https://vfc3eyv16g.execute-api.us-east-1.amazonaws.com/dev/bulk_tx'
+        'https://koklizm6jc.execute-api.us-east-1.amazonaws.com/prod/bulk_get_tx_info'
     {% else %}
-        'https://vfc3eyv16g.execute-api.us-east-1.amazonaws.com/dev/bulk_tx'
+        'https://vfc3eyv16g.execute-api.us-east-1.amazonaws.com/dev/bulk_get_tx_info'
     {%- endif %}
 {% endmacro %}
