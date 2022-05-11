8da1bc14d57433e0b0347089dfbe7ac20f19d095
flipside-kellen <kellen@flipsidecrypto.com>
2022-05-11 14:51:29 -0700

    allow identical rows

--- models/thorchain/silver/silver_thorchain__bond_events.sql
+++ models/thorchain/silver/silver_thorchain__bond_events.sql
@@ -7,9 +7,6 @@ SELECT
   *
 FROM
   {{ ref('thorchain_dbt__bond_events') }}
-  qualify(ROW_NUMBER() over(PARTITION BY tx, chain, from_addr, to_addr, asset, bond_type, memo, block_timestamp
-ORDER BY
-  __HEVO__INGESTED_AT DESC)) = 1
 
 {% if is_incremental() %}
 AND __HEVO_loaded_at >= (
