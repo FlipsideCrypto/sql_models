7d0fb86fcb8a73957f2d9035d6cc4ae74ac2b6e7
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-06-02 12:59:27 -0700

    - remove time range from silver view

--- models/algorand/api/silver_algorand__get_tx.sql
+++ models/algorand/api/silver_algorand__get_tx.sql
@@ -10,11 +10,6 @@ WITH tx AS (
     tx_id
   FROM
     {{ ref("silver_algorand__transactions") }}
-  WHERE
-    block_timestamp BETWEEN '2020-05-15'
-    AND '2020-07-01'
-  ORDER BY
-    block_timestamp DESC
 )
 SELECT
   account_id,
