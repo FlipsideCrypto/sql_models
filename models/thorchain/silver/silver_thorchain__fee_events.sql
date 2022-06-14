71f8937d88f61e7bebd690f7563985bf718ea736
flipside-kellen <kellen@flipsidecrypto.com>
2022-06-13 18:48:22 -0700

    identical rows in fee_events

--- models/thorchain/silver/silver_thorchain__fee_events.sql
+++ models/thorchain/silver/silver_thorchain__fee_events.sql
@@ -3,17 +3,32 @@
   tags = ['snowflake', 'silver_thorchain', 'fee_events']
 ) }}
 
-SELECT
-  *
+WITH base AS (
+  SELECT 
+    f.asset,
+    f.asset_e8,
+    f.pool_deduct,
+    f.block_timestamp,
+    f.tx,
+    p.cnt,
+    ROW_NUMBER() OVER (PARTITION BY f.asset, f.asset_e8, f.pool_deduct, f.block_timestamp, f.tx ORDER BY p.cnt) AS rn
+  FROM
+    {{ ref('thorchain_dbt__fee_events') }}
+    f
+    JOIN {{ ref('thorchain_dbt__fee_events_pk_count') }}
+    p
+    ON p.asset = f.asset 
+    AND p.asset_e8 = f.asset_e8
+    AND p.pool_deduct = f.pool_deduct
+    AND p.block_timestamp = f.block_timestamp
+    AND p.tx = f.tx
+)
+SELECT 
+  asset,
+  asset_e8,
+  pool_deduct,
+  block_timestamp,
+  tx
 FROM
-  {{ ref('thorchain_dbt__fee_events') }}
-
-{% if is_incremental() %}
-WHERE
-  __HEVO_loaded_at >= (
-    SELECT
-      MAX(__HEVO_loaded_at)
-    FROM
-      {{ this }}
-  )
-{% endif %}
+  base
+  WHERE rn <= cnt
