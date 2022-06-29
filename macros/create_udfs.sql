6163801ef224f9a3b133334855d99397dfd11bbd
Eric Laurello <eric.laurello@flipsidecrypto.com>
2022-06-29 13:32:02 -0400

    fix udf

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,14 +1,2 @@
 {% macro create_udfs() %}
-    {% set sql %}
-    {{ udf_bulk_get_asset_metadata() }};
-    {% endset %}
-    {% do run_query(sql) %}
-    {% set sql %}
-    {{ udf_bulk_get_balances() }};
-    {% endset %}
-    {% do run_query(sql) %}
-    {% set sql %}
-    {{ udf_bulk_get_validator_metadata() }};
-    {% endset %}
-    {% do run_query(sql) %}
 {% endmacro %}
