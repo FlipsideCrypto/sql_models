d2ba8b4d74ddbd7f1bebcf57f043a637544bf4cd
jhuhnke <jessica@flipsidecrypto.com>
2022-08-16 09:17:29 -0500

    clean up of crosschain address labels model

--- models/crosschain/silver/silver_crosschain__address_labels.sql
+++ models/crosschain/silver/silver_crosschain__address_labels.sql
@@ -2,10 +2,125 @@
   materialized = 'incremental',
   unique_key = "CONCAT_WS('-', blockchain, address, creator)",
   incremental_strategy = 'delete+insert',
-  cluster_by = ['blockchain', 'address'],
   tags = ['snowflake', 'crosschain', 'address_labels', 'silver_crosschain__address_labels']
 ) }}
 
+WITH base_tables AS (
+
+  SELECT
+    *,
+    SPLIT(
+      SUBSTR(
+        record_metadata :key :: STRING,
+        2,
+        len(
+          record_metadata :key :: STRING
+        ) -2
+      ),
+      '-'
+    ) [1] :: STRING AS blockchain,
+    TO_TIMESTAMP(
+      SPLIT(
+        SUBSTR(
+          record_metadata :key :: STRING,
+          2,
+          len(
+            record_metadata :key :: STRING
+          ) -2
+        ),
+        '-'
+      ) [2] :: INT
+    ) AS insert_date
+  FROM
+    {{ source(
+      'bronze',
+      'prod_address_label_sink_291098491'
+    ) }}
+  WHERE
+    ARRAY_SIZE(
+      SPLIT(
+        SUBSTR(
+          record_metadata :key :: STRING,
+          2,
+          len(
+            record_metadata :key :: STRING
+          ) -2
+        ),
+        '-'
+      )
+    ) = 3
+    AND SPLIT(
+      SUBSTR(
+        record_metadata :key :: STRING,
+        2,
+        len(
+          record_metadata :key :: STRING
+        ) -2
+      ),
+      '-'
+    ) [0] = 'labels'
+
+{% if is_incremental() %}
+AND (
+  record_metadata :CreateTime :: INT / 1000
+) :: TIMESTAMP :: DATE >= (
+  SELECT
+    DATEADD('day', -1, MAX(system_created_at :: DATE))
+  FROM
+    {{ this }}
+)
+{% endif %}
+),
+delete_table AS (
+  SELECT
+    CASE
+      WHEN blockchain IN (
+        'algorand',
+        'solana'
+      ) THEN t.value :address :: STRING
+      ELSE LOWER(
+        t.value :address :: STRING
+      )
+    END AS address,
+    t.value :delete :: STRING AS delete_flag
+  FROM
+    base_tables,
+    LATERAL FLATTEN(
+      input => record_content
+    ) t
+  WHERE
+    delete_flag = 'true'
+),
+flat_table AS (
+  SELECT
+    (
+      record_metadata :CreateTime :: INT / 1000
+    ) :: TIMESTAMP AS system_created_at,
+    insert_date,
+    blockchain,
+    CASE
+      WHEN blockchain IN (
+        'algorand',
+        'solana'
+      ) THEN t.value :address :: STRING
+      ELSE LOWER(
+        t.value :address :: STRING
+      )
+    END AS address,
+    t.value :creator :: STRING AS creator,
+    t.value :l1_label :: STRING AS l1_label,
+    t.value :l2_label :: STRING AS l2_label,
+    t.value :address_name :: STRING AS address_name,
+    t.value :project_name :: STRING AS project_name,
+    t.value :delete :: STRING AS delete_flag
+  FROM
+    base_tables,
+    LATERAL FLATTEN(
+      input => record_content
+    ) t qualify(ROW_NUMBER() over(PARTITION BY blockchain, address, creator
+  ORDER BY
+    insert_date DESC)) = 1
+)
 SELECT
   system_created_at,
   insert_date,
@@ -14,18 +129,18 @@ SELECT
   creator,
   l1_label,
   l2_label,
-  address_name, 
-  project_name
+  address_name,
+  project_name,
+  delete_flag
 FROM
-  {{ ref('silver_dbt__address_labels') }}
-
+  flat_table
 WHERE
-  1 = 1
-{% if is_incremental() %}
-AND system_created_at :: DATE >= (
-  SELECT
-    DATEADD('day', -1, MAX(system_created_at :: DATE))
-  FROM
-    {{ this }} AS address_labels
-)
-{% endif %}
+  address NOT IN (
+    SELECT
+      address
+    FROM
+      delete_table
+  )
+  AND project_name IS NOT NULL
+  AND address_name IS NOT NULL
+  AND l1_label <> 'project'
