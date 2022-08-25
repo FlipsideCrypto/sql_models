66ee5493a8b98638b839ff1993f15ab4024e62c9
Eric Laurello <eric.laurello@flipsidecrypto.com>
2022-08-25 16:29:28 -0400

    check in

--- models/algorand/silver/silver_algorand__swaps_humble_swap_dex.sql
+++ models/algorand/silver/silver_algorand__swaps_humble_swap_dex.sql
@@ -14,7 +14,8 @@ WITH hs_tx_group_ids AS (
         A.tx_group_id,
         A.app_id,
         A.sender AS swapper,
-        A._INSERTED_TIMESTAMP
+        A._INSERTED_TIMESTAMP,
+        COUNT(1) xfer_count
     FROM
         {{ ref('silver_algorand__application_call_transaction') }} A
         JOIN {{ ref('silver_algorand__transfers') }}
@@ -51,8 +52,6 @@ GROUP BY
     A.app_id,
     A.sender,
     A._INSERTED_TIMESTAMP
-HAVING
-    COUNT(1) = 2
 ),
 hs_transfers AS(
     SELECT
@@ -71,30 +70,124 @@ hs_transfers AS(
     WHERE
         A.block_timestamp :: DATE >= '2022-04-22'
         AND amount > 0
-)
+        AND b.xfer_count = 2
+),
+normal_swaps AS (
+    SELECT
+        A.block_timestamp,
+        A.block_id,
+        A.intra,
+        A.tx_group_id,
+        A.app_id,
+        A.swapper,
+        b.asset_id AS swap_from_asset_id,
+        b.amount :: FLOAT AS swap_from_amount,
+        C.pool_address,
+        C.asset_id AS swap_to_asset_id,
+        C.amount :: FLOAT AS swap_to_amount,
+        concat_ws(
+            '-',
+            A.block_id :: STRING,
+            A.intra :: STRING
+        ) AS _unique_key,
+        _INSERTED_TIMESTAMP
+    FROM
+        hs_tx_group_ids A
+        JOIN hs_transfers b
+        ON A.tx_group_id = b.tx_group_id
+        AND b.pool_address IS NULL
+        JOIN hs_transfers C
+        ON A.tx_group_id = C.tx_group_id
+        AND C.pool_address IS NOT NULL
+    WHERE
+        A.xfer_count = 2
+),
+odd_hs_transfers AS (
+    SELECT
+        A.tx_group_id,
+        A.asset_sender,
+        A.asset_id,
+        A.amount,
+        A.receiver,
+        CASE
+            WHEN A.asset_sender <> b.swapper THEN A.asset_sender
+        END AS pool_address
+    FROM
+        {{ ref('silver_algorand__transfers') }} A
+        LEFT JOIN normal_swaps b
+        ON A.tx_group_id = b.tx_group_id
+    WHERE
+        b.xfer_count > 3
+        AND b.tx_group_id IS NULL
+        AND (
+            receiver IN (
+                SELECT
+                    pool_address
+                FROM
+                    normal_swaps
+
+{% if is_incremental() %}
+UNION ALL
 SELECT
-    A.block_timestamp,
-    A.block_id,
-    A.intra,
-    A.tx_group_id,
-    A.app_id,
-    A.swapper,
-    b.asset_id AS swap_from_asset_id,
-    b.amount :: FLOAT AS swap_from_amount,
-    C.pool_address,
-    C.asset_id AS swap_to_asset_id,
-    C.amount :: FLOAT AS swap_to_amount,
-    concat_ws(
-        '-',
-        A.block_id :: STRING,
-        A.intra :: STRING
-    ) AS _unique_key,
-    _INSERTED_TIMESTAMP
+    pool_address
 FROM
-    hs_tx_group_ids A
-    JOIN hs_transfers b
-    ON A.tx_group_id = b.tx_group_id
-    AND b.pool_address IS NULL
-    JOIN hs_transfers C
-    ON A.tx_group_id = C.tx_group_id
-    AND C.pool_address IS NOT NULL
+    {{ this }}
+{% endif %}
+)
+OR asset_sender IN (
+    SELECT
+        pool_address
+    FROM
+        normal_swaps
+
+{% if is_incremental() %}
+UNION ALL
+SELECT
+    pool_address
+FROM
+    {{ this }}
+{% endif %}
+)
+)
+
+{% if is_incremental() %}
+AND A._INSERTED_TIMESTAMP >= (
+    SELECT
+        MAX(
+            _INSERTED_TIMESTAMP
+        )
+    FROM
+        {{ this }}
+) - INTERVAL '4 HOURS'
+{% endif %}
+),
+odd_swaps AS (
+    SELECT
+        A.block_timestamp,
+        A.block_id,
+        A.intra,
+        A.tx_group_id,
+        A.app_id,
+        A.swapper,
+        b.asset_id AS swap_from_asset_id,
+        b.amount :: FLOAT AS swap_from_amount,
+        C.pool_address,
+        C.asset_id AS swap_to_asset_id,
+        C.amount :: FLOAT AS swap_to_amount,
+        concat_ws(
+            '-',
+            A.block_id :: STRING,
+            A.intra :: STRING
+        ) AS _unique_key,
+        _INSERTED_TIMESTAMP
+    FROM
+        hs_tx_group_ids A
+        JOIN odd_hs_transfers b
+        ON A.tx_group_id = b.tx_group_id
+        AND b.pool_address IS NULL
+        JOIN odd_hs_transfers C
+        ON A.tx_group_id = C.tx_group_id
+        AND C.pool_address IS NOT NULL
+    WHERE
+        A.xfer_count > 3
+)
